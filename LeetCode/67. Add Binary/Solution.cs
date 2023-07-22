45be35f4f16cfe88533c83fe53f33a987062316e
coffeek <ya.kofeek@yandex.ru>
2023-07-22 23:39:56 +0400

    67. Add Binary

---
+++ LeetCode/67. Add Binary/Solution.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Text;
+
+namespace LeetCode._67._Add_Binary;
+
+public class Solution
+{
+  public string AddBinary(string a, string b)
+  {
+    var result = new char[Math.Max(a.Length, b.Length) + 1];
+    var carry = 0;
+    var i = a.Length - 1;
+    var j = b.Length - 1;
+    var k = result.Length;
+    
+    while (i >= 0 || j >= 0)
+    {
+      var s = carry;
+      if (i >= 0)
+        s += a[i--] - '0';
+      if (j >= 0)
+        s += b[j--] - '0';
+      var d = s & 1;
+      result[--k] = (char)(d + '0');
+      carry = s >> 1;
+    }
+    if (carry != 0)
+      result[--k] = '1';
+    return new string(result.AsSpan(k));
+  }
+}
---
+++ LeetCode/67. Add Binary/Tests.cs
@@ -0,0 +1,15 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+
+namespace LeetCode._67._Add_Binary;
+
+[TestFixture]
+public class Tests
+{
+  [Test]
+  public void Test1()
+  {
+    new Solution().AddBinary("11", "1").Should().Be("100");
+    new Solution().AddBinary("1010", "1011").Should().Be("10101");
+  }
+}
