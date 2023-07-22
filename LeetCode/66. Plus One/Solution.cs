f16ada8ce00135246694e9f42bb592fe847bbc72
coffeek <ya.kofeek@yandex.ru>
2023-07-22 23:12:54 +0400

    66. Plus One

---
+++ LeetCode/66. Plus One/Solution.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+
+namespace LeetCode._66._Plus_One;
+
+public class Solution
+{
+  public int[] PlusOne(int[] digits)
+  {
+    var result = new Stack<int>(digits.Length + 1);
+    var carry = 1;
+    for (var i = digits.Length - 1; i >= 0; i--)
+    {
+      var s = digits[i] + carry;
+      result.Push(s % 10);
+      carry = s / 10;
+    }
+    if (carry != 0)
+      result.Push(carry);
+    return result.ToArray();
+  }
+}
---
+++ LeetCode/66. Plus One/Tests.cs
@@ -0,0 +1,16 @@
+﻿using NUnit.Framework;
+
+namespace LeetCode._66._Plus_One;
+
+[TestFixture]
+public class Tests
+{
+  [Test]
+  public void Test1()
+  {
+    var sln = new Solution();
+    CollectionAssert.AreEqual(new[] { 1, 2, 4 }, sln.PlusOne(new[] { 1, 2, 3 }));
+    CollectionAssert.AreEqual(new[] { 1, 0, 0, 0 }, sln.PlusOne(new[] { 9, 9, 9 }));
+    CollectionAssert.AreEqual(new[] { 1, 0 }, sln.PlusOne(new[] { 9 }));
+  }
+}
