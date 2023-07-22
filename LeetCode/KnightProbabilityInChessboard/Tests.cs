a8b725d572de2f1750d9c496c3f73635c8a3c101
coffeek <ya.kofeek@yandex.ru>
2023-07-22 21:55:52 +0400

    688. Knight Probability in Chessboard

---
+++ LeetCode/KnightProbabilityInChessboard/Tests.cs
@@ -0,0 +1,16 @@
+﻿using NUnit.Framework;
+
+namespace LeetCode.KnightProbabilityInChessboard;
+
+[TestFixture]
+public class Tests
+{
+  [Test]
+  public void Test1()
+  {
+    var sln = new Solution();
+    Assert.That(sln.KnightProbability(3, 2, 0, 0), Is.EqualTo(0.06250));
+    Assert.That(sln.KnightProbability(1, 0, 0, 0), Is.EqualTo(1));
+    Assert.That(sln.KnightProbability(8, 30, 6, 4), Is.EqualTo(0.00019));
+  }
+}
