e1941fbfcd486e1776fcde4181faa4323bdd3369
coffeek <ya.kofeek@yandex.ru>
2023-07-22 22:47:44 +0400

    27. Remove Element

---
+++ LeetCode/27. Remove Element/Solution.cs
@@ -0,0 +1,14 @@
+﻿namespace LeetCode._27._Remove_Element;
+
+public class Solution
+{
+  public int RemoveElement(int[] nums, int val)
+  {
+    var n = nums.Length;
+    var k = 0;
+    for (var i = 0; i < n; i++)
+      if (nums[i] != val)
+        nums[k++] = nums[i];
+    return k;
+  }
+}
---
+++ LeetCode/27. Remove Element/Tests.cs
@@ -0,0 +1,27 @@
+﻿using NUnit.Framework;
+
+namespace LeetCode._27._Remove_Element;
+
+[TestFixture]
+public class Tests
+{
+  [Test]
+  public void Test1()
+  {
+    var sln = new Solution();
+    var nums = new[] { 3, 2, 2, 3 };
+    var result = sln.RemoveElement(nums, 3);
+    Assert.AreEqual(2, result);
+    CollectionAssert.AreEqual(new[] { 2, 2, 2, 3 }, nums);
+  }
+
+  [Test]
+  public void Test2()
+  {
+    var sln = new Solution();
+    var nums = new[] { 0, 1, 2, 2, 3, 0, 4, 2 };
+    var result = sln.RemoveElement(nums, 2);
+    Assert.AreEqual(5, result);
+    CollectionAssert.AreEqual(new[] { 0, 1, 3, 0, 4, 0, 4, 2 }, nums);
+  }
+}
