0f05ec08d21824689f451c582d8c9210a7d6bd67
coffeek <ya.kofeek@yandex.ru>
2023-07-25 20:56:17 +0400

    852. Peak Index in a Mountain Array

---
+++ LeetCode/852. Peak Index in a Mountain Array/Solution.cs
@@ -0,0 +1,46 @@
+﻿namespace LeetCode._852._Peak_Index_in_a_Mountain_Array;
+
+public class Solution
+{
+  public int PeakIndexInMountainArray(int[] a)
+  {
+    var i = 0;
+    while (a[i] < a[i + 1])
+      i++;
+    return i;
+  }
+  
+  public int PeakIndexInMountainArray_TernarySearch(int[] arr)
+  {
+    var n = arr.Length;
+    var l = 0;
+    var r = n - 1;
+    while (r - l > 1)
+    {
+      var d = (r - l + 1) / 3;
+      var m1 = l + d;
+      var m2 = r - d;
+      if (arr[m1] <= arr[m2])
+        l = m1;
+      else
+        r = m2;
+    }
+    return arr[l] >= arr[r] ? l : r;
+  }
+  
+  public int PeakIndexInMountainArray_BinarySearch(int[] a)
+  {
+    var n = a.Length;
+    var l = 0;
+    var r = n - 1;
+    while (l < r)
+    {
+      var m = l + (r - l) / 2;
+      if (a[m] < a[m + 1])
+        l = m + 1;
+      else
+        r = m;
+    }
+    return l;
+  }
+}
---
+++ LeetCode/852. Peak Index in a Mountain Array/Tests.cs
@@ -0,0 +1,16 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+
+namespace LeetCode._852._Peak_Index_in_a_Mountain_Array;
+
+[TestFixture]
+public class Tests
+{
+  [Test]
+  public void Test1()
+  {
+    new Solution().PeakIndexInMountainArray(new[] { 0, 1, 0 }).Should().Be(1);
+    new Solution().PeakIndexInMountainArray(new[] { 0, 2, 1, 0 }).Should().Be(1);
+    new Solution().PeakIndexInMountainArray(new[] { 0, 10, 5, 2 }).Should().Be(1);
+  }
+}
