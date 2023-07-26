3aaa769d0b3912246ea1cb0b05180737909f4f77
coffeek <ya.kofeek@yandex.ru>
2023-07-27 02:39:54 +0400

    7. Reverse Integer

---
+++ LeetCode/7. Reverse Integer/Tests.cs
@@ -0,0 +1,20 @@
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace LeetCode._7._Reverse_Integer;
+
+[TestFixture]
+public class Tests
+{
+  [TestCase(123, 321)]
+  [TestCase(-123, -321)]
+  [TestCase(120, 21)]
+  [TestCase(int.MaxValue, 0)]
+  [TestCase(int.MinValue, 0)]
+  [TestCase(1463847412, 2147483641)]
+  [TestCase(-2147483648, 0)]
+  public void Test1(int value, int expected)
+  {
+    new Solution().Reverse(value).Should().Be(expected);
+  }
+}
