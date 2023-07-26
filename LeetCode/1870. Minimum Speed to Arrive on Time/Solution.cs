1bd60cadefbbc3aab48f0e98c41e889d9b092e16
coffeek <ya.kofeek@yandex.ru>
2023-07-26 22:17:10 +0400

    1870. Minimum Speed to Arrive on Time

---
+++ LeetCode/1870. Minimum Speed to Arrive on Time/Solution.cs
@@ -0,0 +1,31 @@
+using System;
+
+namespace LeetCode._1870._Minimum_Speed_to_Arrive_on_Time;
+
+public class Solution
+{
+  public int MinSpeedOnTime(int[] dist, double hour)
+  {
+    const int max = 10000001;
+    var vMin = 1;
+    var vMax = max;
+    while (vMin < vMax)
+    {
+      var mv = vMin + (vMax - vMin) / 2;
+      var t = GetTravelTime(dist, mv);
+      if (t <= hour)
+        vMax = mv;
+      else
+        vMin = mv + 1;
+    }
+    return vMax == max ? -1 : vMax;
+  }
+
+  private static double GetTravelTime(int[] dist, int v)
+  {
+    var h = 0.0;
+    for (var i = 0; i < dist.Length - 1; i++)
+      h += Math.Ceiling((double)dist[i] / v);
+    return h + (double)dist[^1] / v;
+  }
+}
---
+++ LeetCode/1870. Minimum Speed to Arrive on Time/Tests.cs
@@ -0,0 +1,16 @@
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace LeetCode._1870._Minimum_Speed_to_Arrive_on_Time;
+
+[TestFixture]
+public class Tests
+{
+  [TestCase(new[] { 1, 3, 2 }, 6, 1)]
+  [TestCase(new[] { 1, 3, 2 }, 2.7, 3)]
+  [TestCase(new[] { 1, 3, 2 }, 1.9, -1)]
+  public void Test(int[] dist, double hours, int expected)
+  {
+    new Solution().MinSpeedOnTime(dist, hours).Should().Be(expected);
+  }
+}
