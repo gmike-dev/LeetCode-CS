e55b5b8c013772592e945e99a50381e5d02ddc60
coffeek <ya.kofeek@yandex.ru>
2023-07-27 01:35:45 +0400

    3. Longest Substring Without Repeating Characters

---
+++ LeetCode/3. Longest Substring Without Repeating Characters/Tests.cs
@@ -0,0 +1,18 @@
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace LeetCode._3._Longest_Substring_Without_Repeating_Characters;
+
+[TestFixture]
+public class Tests
+{
+  [TestCase("abcabcbb", 3)]
+  [TestCase("bbbbb", 1)]
+  [TestCase("pwwkew", 3)]
+  [TestCase("", 0)]
+  [TestCase("dvdf", 3)]
+  public void Test1(string s, int expected)
+  {
+    new Solution().LengthOfLongestSubstring(s).Should().Be(expected);
+  }
+}
