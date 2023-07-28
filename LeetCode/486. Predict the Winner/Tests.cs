8ad1010ca5a4833855d949ccda22805010f0c708
coffeek <ya.kofeek@yandex.ru>
2023-07-29 00:25:44 +0400

    2141. Maximum Running Time of N Computers (using DP)

--- LeetCode/486. Predict the Winner/Tests.cs
+++ LeetCode/486. Predict the Winner/Tests.cs
@@ -8,8 +8,15 @@ public class Tests
 {
   [TestCase(new[] { 1, 5, 2 }, false)]
   [TestCase(new[] { 1, 5, 233, 7 }, true)]
-  public void Test1(int[] nums, bool result)
+  public void TestDp(int[] nums, bool result)
   {
     new Solution().PredictTheWinner(nums).Should().Be(result);
+  }
+  
+  [TestCase(new[] { 1, 5, 2 }, false)]
+  [TestCase(new[] { 1, 5, 233, 7 }, true)]
+  public void TestRecursion(int[] nums, bool result)
+  {
+    new Solution().PredictTheWinner_Recursion(nums).Should().Be(result);
   }
 }
