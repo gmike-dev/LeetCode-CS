ce2f059f7a29ae6f20468db2d2fb8283113b3394
coffeek <ya.kofeek@yandex.ru>
2023-07-29 20:44:28 +0400

    808. Soup Servings

---
+++ LeetCode/808. Soup Servings/Solution.cs
@@ -0,0 +1,35 @@
+using System.Collections.Generic;
+
+namespace LeetCode._808._Soup_Servings;
+
+public class Solution
+{
+  private readonly Dictionary<(int, int), double> _cache = new();
+
+  public double SoupServings(int n)
+  {
+    if (n > 4800)
+      return 1.0;
+
+    n = (n + 24) / 25;
+    return Probability(n, n);
+  }
+
+  private double Probability(int firstServes, int secondServes)
+  {
+    if (firstServes <= 0 && secondServes <= 0)
+      return 0.5;
+    if (firstServes <= 0)
+      return 1.0;
+    if (secondServes <= 0)
+      return 0.0;
+    var cacheKey = (firstServes, secondServes);
+    if (_cache.TryGetValue(cacheKey, out var dp))
+      return dp;
+    _cache[cacheKey] = (Probability(firstServes - 4, secondServes) +
+                        Probability(firstServes - 3, secondServes - 1) +
+                        Probability(firstServes - 2, secondServes - 2) +
+                        Probability(firstServes - 1, secondServes - 3)) * 0.25;
+    return _cache[cacheKey];
+  }
+}
