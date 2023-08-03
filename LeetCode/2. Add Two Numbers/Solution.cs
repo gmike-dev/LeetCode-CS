1fe901e6f0d60a0e1a9dccf34b5dd70be94885fb
coffeek <ya.kofeek@yandex.ru>
2023-08-03 22:27:01 +0400

    2. Add Two Numbers

---
+++ LeetCode/2. Add Two Numbers/Solution.cs
@@ -0,0 +1,35 @@
+namespace LeetCode._2._Add_Two_Numbers;
+
+public class ListNode
+{
+  public int val;
+  public ListNode next;
+
+  public ListNode(int val = 0, ListNode next = null)
+  {
+    this.val = val;
+    this.next = next;
+  }
+}
+
+public class Solution
+{
+  public ListNode AddTwoNumbers(ListNode l1, ListNode l2)
+  {
+    var carry = 0;
+    var result = new ListNode();
+    var node = result;
+    while (l1 != null || l2 != null || carry != 0)
+    {
+      var x = l1?.val ?? 0;
+      var y = l2?.val ?? 0;
+      var s = x + y + carry;
+      carry = s / 10;
+      node.next = new ListNode(s % 10);
+      node = node.next = new ListNode(s % 10);
+      l1 = l1?.next;
+      l2 = l2?.next;
+    }
+    return result.next;
+  }
+}
