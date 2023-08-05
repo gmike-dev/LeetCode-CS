e645f7acd791570efdb222df64053453f4fa1519
coffeek <ya.kofeek@yandex.ru>
2023-08-05 21:40:30 +0400

    95. Unique Binary Search Trees II

---
+++ LeetCode/95. Unique Binary Search Trees II/Solution.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+public class TreeNode
+{
+  public int val;
+  public TreeNode left;
+  public TreeNode right;
+
+  public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
+  {
+    this.val = val;
+    this.left = left;
+    this.right = right;
+  }
+}
+
+public class Solution
+{
+  public IList<TreeNode> GenerateTrees(int n)
+  {
+    return GenerateTrees(1, n).ToArray();
+  }
+
+  private IEnumerable<TreeNode> GenerateTrees(int begin, int end)
+  {
+    if (begin > end)
+      yield return null;
+    else if (begin == end)
+      yield return new TreeNode(begin);
+    else
+    {
+      for (var i = begin; i <= end; i++)
+      {
+        foreach (var leftNode in GenerateTrees(begin, i - 1))
+        foreach (var rightNode in GenerateTrees(i + 1, end))
+          yield return new TreeNode(i, leftNode, rightNode);
+      }
+    }
+  }
+}
---
+++ LeetCode/95. Unique Binary Search Trees II/Tests.cs
@@ -0,0 +1,25 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+
+namespace LeetCode._95._Unique_Binary_Search_Trees_II;
+
+[TestFixture]
+public class Tests
+{
+  [Test]
+  public void Test1()
+  {
+    new Solution().GenerateTrees(1).Should()
+      .BeEquivalentTo(new[] { new TreeNode(1) });
+
+    new Solution().GenerateTrees(3).Should()
+      .BeEquivalentTo(new[]
+      {
+        new TreeNode(1, null, new TreeNode(2, null, new TreeNode(3))),
+        new TreeNode(1, null, new TreeNode(3, new TreeNode(2))),
+        new TreeNode(2, new TreeNode(1), new TreeNode(3)),
+        new TreeNode(3, new TreeNode(2, new TreeNode(1))),
+        new TreeNode(3, new TreeNode(1, null, new TreeNode(2)))
+      }, config => config.WithoutStrictOrdering());
+  }
+}
