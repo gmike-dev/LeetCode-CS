79de196a14089d5d94aa7efb38257fbe99ed9bbd
coffeek <ya.kofeek@yandex.ru>
2023-08-06 11:48:44 +0400

    70. Climbing Stairs

---
+++ LeetCode/70. Climbing Stairs/Solution.cs
@@ -0,0 +1,16 @@
+﻿namespace LeetCode._70._Climbing_Stairs;
+
+public class Solution
+{
+  public int ClimbStairs(int n)
+  {
+    if (n == 1)
+      return 1;
+
+    var a = 1;
+    var b = 2;
+    for (var i = 3; i <= n; i++)
+      (b, a) = (a + b, b);
+    return b;
+  }
+}
---
+++ LeetCode/70. Climbing Stairs/Tests.cs
@@ -0,0 +1,15 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+
+namespace LeetCode._70._Climbing_Stairs;
+
+[TestFixture]
+public class Tests
+{
+  [TestCase(2, 2)]
+  [TestCase(3, 3)]
+  public void Test1(int n, int expected)
+  {
+    new Solution().ClimbStairs(n).Should().Be(expected);
+  }
+}
