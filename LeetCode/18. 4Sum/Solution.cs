4754351d202bb8f1323fbbbae98bcdac5df7a5ec
coffeek <ya.kofeek@yandex.ru>
2023-08-07 01:06:27 +0400

    18. 4Sum

---
+++ LeetCode/18. 4Sum/Solution.cs
@@ -0,0 +1,51 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LeetCode._18._4Sum;
+
+public class Solution
+{
+  public IList<IList<int>> FourSum(int[] nums, int target)
+  {
+    Array.Sort(nums);
+    var result = new List<IList<int>>();
+    for (var i = 0; i < nums.Length - 3; i++)
+    {
+      if (i > 0 && nums[i - 1] == nums[i])
+        continue;
+      for (var j = i + 1; j < nums.Length - 2; j++)
+      {
+        if (j > i + 1 && nums[j - 1] == nums[j])
+          continue;
+        result.AddRange(TwoSum(nums, (long)target - nums[i] - nums[j], j + 1)
+          .Select(pair => new[] { nums[i], nums[j], pair.Item1, pair.Item2 }));
+      }
+    }
+    return result;
+  }
+
+  private static IEnumerable<(int, int)> TwoSum(int[] nums, long target, int start)
+  {
+    var k = start;
+    var l = nums.Length - 1;
+    while (k < l)
+    {
+      long s = nums[k] + nums[l];
+      if (s < target)
+        k++;
+      else if (s > target)
+        l--;
+      else
+      {
+        yield return (nums[k], nums[l]);
+        while (k < l && nums[k] == nums[k + 1])
+          k++;
+        while (k < l && nums[l - 1] == nums[l])
+          l--;
+        k++;
+        l--;
+      }
+    }
+  }
+}
---
+++ LeetCode/18. 4Sum/Tests.cs
@@ -0,0 +1,36 @@
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace LeetCode._18._4Sum;
+
+[TestFixture]
+public class Tests
+{
+  [Test]
+  public void Test1()
+  {
+    new Solution().FourSum(new[] { 1, 0, -1, 0, -2, 2 }, 0).Should().BeEquivalentTo(
+      new[] { new[] { -2, -1, 1, 2 }, new[] { -2, 0, 0, 2 }, new[] { -1, 0, 0, 1 } });
+  }
+
+  [Test]
+  public void Test2()
+  {
+    new Solution().FourSum(new[] { 2, 2, 2, 2, 2 }, 8).Should().BeEquivalentTo(
+      new[] { new[] { 2, 2, 2, 2 } });
+  }
+
+  [Test]
+  public void Test3()
+  {
+    new Solution().FourSum(new[] { 1, -2, -5, -4, -3, 3, 3, 5 }, -11).Should().BeEquivalentTo(
+      new[] { new[] { -5, -4, -3, 1 } });
+  }
+
+  [Test]
+  public void Test4()
+  {
+    new Solution().FourSum(new[] { 1000000000, 1000000000, 1000000000, 1000000000 }, -294967296).Should()
+      .BeEmpty();
+  }
+}
