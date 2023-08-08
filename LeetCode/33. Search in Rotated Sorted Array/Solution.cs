d05ca499e23c901e50e53b1ce83b907dc0454bd6
Городилов Михаил (Gorodilov_MA) <Gorodilov_MA@directum.ru>
2023-08-08 10:19:14 +0400

    33. Search in Rotated Sorted Array

---
+++ LeetCode/33. Search in Rotated Sorted Array/Solution.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace LeetCode._33._Search_in_Rotated_Sorted_Array;
+
+public class Solution
+{
+  public int Search(int[] nums, int target)
+  {
+    return Search(nums, 0, nums.Length - 1, target);
+  }
+
+  private int Search(int[] nums, int l, int r, int target)
+  {
+    if (l > r)
+      return -1;
+
+    if (l == r)
+      return nums[l] == target ? l : -1;
+
+    int result;
+    if (nums[l] >= nums[r])
+    {
+      var m = l + (r - l) / 2;
+      result = Search(nums, l, m, target);
+      if (result >= 0)
+        return result;
+      result = Search(nums, m + 1, r, target);
+      return result >= 0 ? result : -1;
+    }
+    
+    result = Array.BinarySearch(nums, l, r - l + 1, target);
+    return result < 0 ? -1 : result;
+  }
+}
---
+++ LeetCode/33. Search in Rotated Sorted Array/Tests.cs
@@ -0,0 +1,19 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+
+namespace LeetCode._33._Search_in_Rotated_Sorted_Array;
+
+[TestFixture]
+public class Tests
+{
+  [TestCase(new[] { 4, 5, 6, 7, 0, 1, 2 }, 0, 4)]
+  [TestCase(new[] { 4, 5, 6, 7, 0, 1, 2 }, 3, -1)]
+  [TestCase(new[] { 1 }, 0, -1)]
+  [TestCase(new[] { 1, 1, 2, 1, 1, 1 }, 2, 2)]
+  [TestCase(new[] { 1, 1, 2, 1, 1, 1, 1 }, 2, 2)]
+  [TestCase(new[] { 1, 1, 1, 1, 2, 1, 1 }, 2, 4)]
+  public void Test1(int[] nums, int target, int expected)
+  {
+    new Solution().Search(nums, target).Should().Be(expected);
+  }
+}
