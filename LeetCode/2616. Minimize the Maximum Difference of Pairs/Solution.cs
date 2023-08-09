6f5b39017c459fd6a90dbff787ba8bfd3d1fa5a5
coffeek <ya.kofeek@yandex.ru>
2023-08-09 21:59:24 +0400

    2616. Minimize the Maximum Difference of Pairs

---
+++ LeetCode/2616. Minimize the Maximum Difference of Pairs/Solution.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace LeetCode._2616._Minimize_the_Maximum_Difference_of_Pairs;
+
+public class Solution
+{
+  public int MinimizeMax(int[] nums, int p)
+  {
+    Array.Sort(nums);
+
+    var l = 0;
+    var r = nums[^1] - nums[0];
+    while (l < r)
+    {
+      var m = l + (r - l) / 2;
+      if (CanTakePairs(nums, p, m))
+        r = m;
+      else
+        l = m + 1;
+    }
+    return l;
+  }
+
+  private static bool CanTakePairs(int[] nums, int p, int d)
+  {
+    for (var i = 0; i < nums.Length - 1 && p > 0; i++)
+    {
+      if (nums[i + 1] - nums[i] <= d)
+      {
+        p--;
+        i++;
+      }
+    }
+    return p == 0;
+  }
+}
---
+++ LeetCode/2616. Minimize the Maximum Difference of Pairs/Tests.cs
@@ -0,0 +1,17 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+
+namespace LeetCode._2616._Minimize_the_Maximum_Difference_of_Pairs;
+
+[TestFixture]
+public class Tests
+{
+  [TestCase(new[] { 10, 1, 2, 7, 1, 3 }, 2, 1)]
+  [TestCase(new[] { 4, 2, 1, 2 }, 1, 0)]
+  [TestCase(new[] { 3, 11, 4, 3, 5, 7, 4, 4, 5, 5 }, 3, 0)]
+  [TestCase(new[] { 3, 0, 5, 0, 0, 1, 6 }, 3, 1)]
+  public void Test1(int[] nums, int p, int expected)
+  {
+    new Solution().MinimizeMax(nums, p).Should().Be(expected);
+  }
+}
