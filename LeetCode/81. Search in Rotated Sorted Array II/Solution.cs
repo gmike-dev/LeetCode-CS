2a96a69444080b1d637c19308382a081c7733e50
coffeek <ya.kofeek@yandex.ru>
2023-08-10 09:21:09 +0400

    81. Search in Rotated Sorted Array II

---
+++ LeetCode/81. Search in Rotated Sorted Array II/Solution.cs
@@ -0,0 +1,32 @@
+using System;
+
+namespace LeetCode._81._Search_in_Rotated_Sorted_Array_II;
+
+public class Solution
+{
+  /// <summary>
+  /// https://leetcode.com/problems/search-in-rotated-sorted-array-ii
+  /// </summary>
+  /// <param name="nums">Integer array sorted in non-decreasing order (not necessarily with distinct values).</param>
+  public bool Search(int[] nums, int target)
+  {
+    return Search(nums, 0, nums.Length - 1, target);
+  }
+
+  private bool Search(int[] nums, int l, int r, int target)
+  {
+    if (l > r)
+      return false;
+
+    if (l == r)
+      return nums[l] == target;
+
+    if (nums[l] >= nums[r])
+    {
+      var m = l + (r - l) / 2;
+      return Search(nums, l, m, target) || Search(nums, m + 1, r, target);
+    }
+    
+    return Array.BinarySearch(nums, l, r - l + 1, target) >= 0;
+  }
+}
---
+++ LeetCode/81. Search in Rotated Sorted Array II/Tests.cs
@@ -0,0 +1,15 @@
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace LeetCode._81._Search_in_Rotated_Sorted_Array_II;
+
+[TestFixture]
+public class Tests
+{
+  [TestCase(new[] { 2, 5, 6, 0, 0, 1, 2 }, 0, true)]
+  [TestCase(new[] { 2, 5, 6, 0, 0, 1, 2 }, 3, false)]
+  public void Test1(int[] nums, int target, bool expected)
+  {
+    new Solution().Search(nums, target).Should().Be(expected);
+  }
+}
