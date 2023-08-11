b193dbc071f71ec7372b860623fa6a640370210e
coffeek <ya.kofeek@yandex.ru>
2023-08-12 00:01:05 +0400

    518. Coin Change II (simple dp)

--- LeetCode/518. Coin Change II/Solution.cs
+++ LeetCode/518. Coin Change II/Solution.cs
@@ -4,9 +4,33 @@ namespace LeetCode._518._Coin_Change_II;
 
 public class Solution
 {
+  public int Change(int amount, int[] coins)
+  {
+    var n = coins.Length;
+
+    var dp = new int[amount + 1][];
+    for (var i = 0; i < dp.Length; i++)
+      dp[i] = new int[n + 1];
+
+    for (var s = 0; s <= amount; s += coins[0])
+      dp[s][0] = 1;
+
+    for (var i = 1; i < n; i++)
+    {
+      for (var s = 0; s <= amount; s++)
+      {
+        if (s >= coins[i])
+          dp[s][i] = dp[s][i - 1] + dp[s - coins[i]][i];
+        else
+          dp[s][i] = dp[s][i - 1];
+      }
+    }
+    return dp[amount][n - 1];
+  }
+  
   private int[][] _cache;
 
-  public int Change(int amount, int[] coins)
+  public int Change_Memoization(int amount, int[] coins)
   {
     _cache = new int[amount + 1][];
     for (var i = 0; i < _cache.Length; i++)
--- LeetCode/518. Coin Change II/Tests.cs
+++ LeetCode/518. Coin Change II/Tests.cs
@@ -9,8 +9,11 @@ public class Tests
   [TestCase(5, new[] { 1, 2, 5 }, 4)]
   [TestCase(3, new[] { 2 }, 0)]
   [TestCase(10, new[] { 10 }, 1)]
+  [TestCase(0, new[] { 1, 2, 5 }, 1)]
+  [TestCase(500, new[] { 1, 2, 5 }, 12701)]
   public void Test1(int amount, int[] coins, int expected)
   {
     new Solution().Change(amount, coins).Should().Be(expected);
+    new Solution().Change_Memoization(amount, coins).Should().Be(expected);
   }
 }
