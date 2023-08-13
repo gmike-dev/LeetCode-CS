9bbb1a00336a93f1f14205d2a2dafd465ca3aecf
coffeek <ya.kofeek@yandex.ru>
2023-08-13 18:25:21 +0400

    2369. Check if There is a Valid Partition For The Array

---
+++ LeetCode/2369. Check if There is a Valid Partition For The Array/Tests.cs
@@ -0,0 +1,16 @@
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace LeetCode._2369._Check_if_There_is_a_Valid_Partition_For_The_Array;
+
+[TestFixture]
+public class Tests
+{
+  [TestCase(new[] { 4, 4, 4, 5, 6 }, true)]
+  [TestCase(new[] { 1, 1, 1, 2 }, false)]
+  public void Test1(int[] nums, bool expected)
+  {
+    new SolutionUsingDp().ValidPartition(nums).Should().Be(expected);
+    new SolutionUsingMemoization().ValidPartition(nums).Should().Be(expected);
+  }
+}
