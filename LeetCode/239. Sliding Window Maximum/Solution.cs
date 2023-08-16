78c2d4401e5ac8f12df7057b1cbfd929fa3a8466
coffeek <ya.kofeek@yandex.ru>
2023-08-16 10:53:51 +0400

    239. Sliding Window Maximum (using MinMaxQueue)

---
+++ LeetCode/239. Sliding Window Maximum/Solution.cs
@@ -0,0 +1,74 @@
+using System;
+using System.Collections.Generic;
+
+namespace LeetCode._239._Sliding_Window_Maximum;
+
+public class Solution
+{
+  public int[] MaxSlidingWindow(int[] nums, int k)
+  {
+    var q = new MaxQueue(k);
+    for (var i = 0; i < k; i++)
+      q.Enqueue(nums[i]);
+    var n = nums.Length;
+    var result = new List<int>(n) { q.Max };
+    for (var i = 0; i < n - k; i++)
+    {
+      q.Dequeue();
+      q.Enqueue(nums[(k + i) % n]);
+      result.Add(q.Max);
+    }
+    return result.ToArray();
+  }
+
+  private class MaxStack
+  {
+    private readonly record struct StackElement(int Value, int Max);
+
+    private readonly Stack<StackElement> _s;
+    
+    public int Count => _s.Count;
+
+    public int Max => _s.TryPeek(out var top) ? top.Max : int.MinValue;
+
+    public void Push(int value)
+    {
+      var se = _s.TryPeek(out var top) ?
+        new StackElement(value, Math.Max(value, top.Max)) :
+        new StackElement(value, value);
+      _s.Push(se);
+    }
+
+    public int Pop() => _s.Pop().Value;
+
+    public MaxStack(int capacity) => _s = new Stack<StackElement>(capacity);
+  }
+
+
+  private class MaxQueue
+  {
+    private readonly MaxStack _front;
+    private readonly MaxStack _back;
+
+    public MaxQueue(int capacity)
+    {
+      _front = new MaxStack(capacity);
+      _back = new MaxStack(capacity);
+    }
+
+    public int Max => Math.Max(_front.Max, _back.Max);
+
+    public void Enqueue(int value) => _front.Push(value);
+
+    public void Dequeue()
+    {
+      if (_back.Count == 0)
+      {
+        while (_front.Count > 0)
+          _back.Push(_front.Pop());
+      }
+      _back.Pop();
+    }
+  }
+  
+}
---
+++ LeetCode/239. Sliding Window Maximum/Tests.cs
@@ -0,0 +1,15 @@
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace LeetCode._239._Sliding_Window_Maximum;
+
+[TestFixture]
+public class Tests
+{
+  [TestCase(new[] { 1, 3, -1, -3, 5, 3, 6, 7 }, 3, new[] { 3, 3, 5, 5, 6, 7 })]
+  [TestCase(new[] { 1 }, 1, new[] { 1 })]
+  public void Test1(int[] nums, int k, int[] expected)
+  {
+    new Solution().MaxSlidingWindow(nums, k).Should().BeEquivalentTo(expected);
+  }
+}
