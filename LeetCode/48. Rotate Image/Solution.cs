7d6da268465694460cefc442ebee293088f91edc
coffeek <ya.kofeek@yandex.ru>
2023-08-20 23:49:45 +0400

    48. Rotate Image

---
+++ LeetCode/48. Rotate Image/Solution.cs
@@ -0,0 +1,25 @@
+using System;
+
+namespace LeetCode._48._Rotate_Image;
+
+public class Solution
+{
+  public void Rotate(int[][] matrix)
+  {
+    Array.Reverse(matrix);
+    var n = matrix.Length;
+    for (var i = 0; i < n - 1; i++)
+    for (var j = i + 1; j < n; j++)
+      (matrix[i][j], matrix[j][i]) = (matrix[j][i], matrix[i][j]);
+  }
+  
+  public void Rotate2(int[][] matrix)
+  {
+    foreach (var row in matrix)
+      Array.Reverse(row);
+    var n = matrix.Length;
+    for (var i = 0; i < n - 1; i++)
+    for (var j = 0; j < n - i - 1; j++)
+      (matrix[i][j], matrix[n - j - 1][n - i - 1]) = (matrix[n - j - 1][n - i - 1], matrix[i][j]);
+  }
+}
---
+++ LeetCode/48. Rotate Image/Tests.cs
@@ -0,0 +1,59 @@
+using System.Collections.Generic;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace LeetCode._48._Rotate_Image;
+
+[TestFixture]
+public class Tests
+{
+  private static IEnumerable<object[]> TestsCases()
+  {
+    yield return new object[]
+    {
+      new[]
+      {
+        new[] { 1, 2, 3 },
+        new[] { 4, 5, 6 },
+        new[] { 7, 8, 9 }
+      },
+      new[]
+      {
+        new[] { 7, 4, 1 },
+        new[] { 8, 5, 2 },
+        new[] { 9, 6, 3 }
+      }
+    };
+    yield return new object[]
+    {
+      new[]
+      {
+        new[] { 5, 1, 9, 11 },
+        new[] { 2, 4, 8, 10 },
+        new[] { 13, 3, 6, 7 },
+        new[] { 15, 14, 12, 16 }
+      },
+      new[]
+      {
+        new[] { 15, 13, 2, 5 },
+        new[] { 14, 3, 4, 1 },
+        new[] { 12, 6, 8, 9 },
+        new[] { 16, 7, 10, 11 }
+      }
+    };
+  }
+
+  [TestCaseSource(nameof(TestsCases))]
+  public void Test1(int[][] matrix, int[][] expected)
+  {
+    new Solution().Rotate(matrix);
+    matrix.Should().BeEquivalentTo(expected);
+  }
+
+  [TestCaseSource(nameof(TestsCases))]
+  public void Test2(int[][] matrix, int[][] expected)
+  {
+    new Solution().Rotate2(matrix);
+    matrix.Should().BeEquivalentTo(expected);
+  }
+}
