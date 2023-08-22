de550df1330401bf65932e784b496306adae54d1
coffeek <ya.kofeek@yandex.ru>
2023-08-22 22:47:43 +0400

    168. Excel Sheet Column Title

---
+++ LeetCode/168. Excel Sheet Column Title/Solution.cs
@@ -0,0 +1,21 @@
+using System.Collections.Generic;
+
+namespace LeetCode._168._Excel_Sheet_Column_Title;
+
+public class Solution
+{
+  public string ConvertToTitle(int columnNumber)
+  {
+    var s = new Stack<char>();
+  
+    while (columnNumber > 0)
+    {
+      columnNumber--;
+      var d = columnNumber % 26;
+      s.Push((char)('A' + d));
+      columnNumber /= 26;
+    }
+
+    return string.Join("", s);
+  }
+}
---
+++ LeetCode/168. Excel Sheet Column Title/Tests.cs
@@ -0,0 +1,16 @@
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace LeetCode._168._Excel_Sheet_Column_Title;
+
+[TestFixture]
+public class Tests
+{
+  [TestCase(1, "A")]
+  [TestCase(28, "AB")]
+  [TestCase(701, "ZY")]
+  public void Test(int columnNumber, string expected)
+  {
+    new Solution().ConvertToTitle(columnNumber).Should().Be(expected);
+  }
+}
