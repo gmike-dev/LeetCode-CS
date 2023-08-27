ee442ae9d2ad7c2347378da1b1ed6dc8e590ac86
coffeek <ya.kofeek@yandex.ru>
2023-08-28 00:14:45 +0400

    415. Add Strings (simplified the code)

--- LeetCode/415. Add Strings/Solution.cs
+++ LeetCode/415. Add Strings/Solution.cs
@@ -7,20 +7,19 @@ public class Solution
 {
   public string AddStrings(string num1, string num2)
   {
-    var result = new List<char>();
     var carry = 0;
     var n = Math.Max(num1.Length, num2.Length);
-    for (var i = 0; i < n || carry > 0; i++)
+    var result = new Stack<char>(n + 1);
+    for (var i = 1; i <= n || carry > 0; i++)
     {
       var s = carry;
-      if (i < num1.Length)
-        s += num1[^(i + 1)] - '0';
-      if (i < num2.Length)
-        s += num2[^(i + 1)] - '0';
-      result.Add((char)(s % 10 + '0'));
+      if (i <= num1.Length)
+        s += num1[^i] - '0';
+      if (i <= num2.Length)
+        s += num2[^i] - '0';
+      result.Push((char)(s % 10 + '0'));
       carry = s / 10;
     }
-    result.Reverse();
-    return string.Join("", result);
+    return new string(result.ToArray());
   }
 }
