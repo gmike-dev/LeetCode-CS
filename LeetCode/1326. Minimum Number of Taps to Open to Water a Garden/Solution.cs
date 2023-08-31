6766849f71e6162c7e73f4d1b0f3f1ecfe00a4a1
coffeek <ya.kofeek@yandex.ru>
2023-08-31 23:27:52 +0400

    1326. Minimum Number of Taps to Open to Water a Garden (greedy)

---
+++ LeetCode/1326. Minimum Number of Taps to Open to Water a Garden/Solution.cs
@@ -0,0 +1,35 @@
+using System;
+using System.Collections.Generic;
+
+namespace LeetCode._1326._Minimum_Number_of_Taps_to_Open_to_Water_a_Garden;
+
+public class Solution
+{
+  public int MinTaps(int n, int[] ranges)
+  {
+    var segments = new List<(int l, int r)>();
+    for (var i = 0; i <= n; i++)
+      segments.Add((Math.Max(0, i - ranges[i]), Math.Min(n, i + ranges[i])));
+    segments.Sort();
+    var curr = 0;
+    while (curr < n && segments[curr].l == segments[curr + 1].l)
+      curr++;
+    var count = 1;
+    while (curr < n && segments[curr].r < n)
+    {
+      var next = curr;
+      for (var i = next + 1; i <= n && segments[i].l <= segments[curr].r; i++)
+      {
+        if (segments[i].r > segments[next].r)
+          next = i;
+      }
+      if (next == curr)
+        return -1;
+      count++;
+      curr = next;
+    }
+    if (segments[curr].r < n)
+      return -1;
+    return count;
+  }
+}
---
+++ LeetCode/1326. Minimum Number of Taps to Open to Water a Garden/Tests.cs
@@ -0,0 +1,15 @@
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace LeetCode._1326._Minimum_Number_of_Taps_to_Open_to_Water_a_Garden;
+
+[TestFixture]
+public class Tests
+{
+  [TestCase(5, new[] { 3, 4, 1, 1, 0, 0 }, 1)]
+  [TestCase(3, new[] { 0, 0, 0, 0 }, -1)]
+  public void Test(int n, int[] ranges, int expected)
+  {
+    new Solution().MinTaps(n, ranges).Should().Be(expected);
+  }
+}
