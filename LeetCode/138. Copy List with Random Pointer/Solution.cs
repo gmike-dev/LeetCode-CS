f3eff62c4466a2adfc45dc5e1aa87a8b2049d9ac
coffeek <ya.kofeek@yandex.ru>
2023-09-05 12:47:28 +0400

    138. Copy List with Random Pointer

---
+++ LeetCode/138. Copy List with Random Pointer/Solution.cs
@@ -0,0 +1,25 @@
+using System.Collections.Generic;
+
+namespace LeetCode._138._Copy_List_with_Random_Pointer;
+
+public class Solution
+{
+  public Node CopyRandomList(Node head)
+  {
+    if (head == null)
+      return null;
+    
+    var copies = new Dictionary<Node, Node>();
+    for (var node = head; node != null; node = node.next)
+      copies[node] = new Node(node.val);
+    for (var node = head; node != null; node = node.next)
+    {
+      if (node.next != null)
+        copies[node].next = copies[node.next];
+      if (node.random != null)
+        copies[node].random = copies[node.random];
+    }
+
+    return copies[head];
+  }
+}
---
+++ LeetCode/138. Copy List with Random Pointer/Tests.cs
@@ -0,0 +1,34 @@
+using System.Collections.Generic;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace LeetCode._138._Copy_List_with_Random_Pointer;
+
+[TestFixture]
+public class Tests
+{
+  [Test]
+  public void Test1()
+  {
+    var list = new List<Node> { new(0), new(1), new(2), new(3) };
+    list[2].random = list[0];
+    list[1].random = list[3];
+    new Solution().CopyRandomList(list[0]).Should().NotBeSameAs(list[0]).And.BeEquivalentTo(list[0]);
+  }
+  
+  [Test]
+  public void Test2()
+  {
+    new Solution().CopyRandomList(null).Should().BeNull();
+  }
+  
+  [Test]
+  public void Test3()
+  {
+    var list = new List<Node> { new(0) };
+    list[0].random = list[0];
+    var result = new Solution().CopyRandomList(list[0]);
+    result.Should().NotBeSameAs(list[0]);
+    result.random.Should().BeSameAs(result);
+  }
+}
