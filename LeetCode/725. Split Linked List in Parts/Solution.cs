1cdb8d5b8c13a60b54db738cc0758f3d57e59727
coffeek <ya.kofeek@yandex.ru>
2023-09-06 10:30:49 +0400

    725. Split Linked List in Parts

---
+++ LeetCode/725. Split Linked List in Parts/Solution.cs
@@ -0,0 +1,29 @@
+namespace LeetCode._725._Split_Linked_List_in_Parts;
+
+public class Solution
+{
+  public ListNode[] SplitListToParts(ListNode head, int k)
+  {
+    var length = 0;
+    for (var node = head; node != null; node = node.next)
+      length++;
+
+    var result = new ListNode[k];
+    var curr = head;
+    var prev = (ListNode)null;
+    for (var i = 0; i < k; i++)
+    {
+      result[i] = curr;
+      var partSize = (length + k - i - 1) / (k - i);
+      for (var j = 0; j < partSize; j++)
+      {
+        prev = curr;
+        curr = curr.next;
+      }
+      if (prev != null)
+        prev.next = null;
+      length -= partSize;
+    }
+    return result;
+  }
+}
