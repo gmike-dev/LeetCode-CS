e2cbc3ec8138e2503d4b5d7f1614544176747960
coffeek <ya.kofeek@yandex.ru>
2023-09-06 21:45:45 +0400

    234. Palindrome Linked List (O(1) memory solution)

---
+++ LeetCode/234. Palindrome Linked List/ListNode.cs
@@ -0,0 +1,13 @@
+namespace LeetCode._234._Palindrome_Linked_List;
+
+public class ListNode
+{
+  public int val;
+  public ListNode next;
+
+  public ListNode(int val = 0, ListNode next = null)
+  {
+    this.val = val;
+    this.next = next;
+  }
+}
---
+++ LeetCode/234. Palindrome Linked List/Solution.cs
@@ -0,0 +1,29 @@
+namespace LeetCode._234._Palindrome_Linked_List;
+
+public class Solution
+{
+  public bool IsPalindrome(ListNode head)
+  {
+    if (head?.next == null)
+      return true;
+
+    var slow = head;
+    var fast = head;
+    var reversed = (ListNode)null;
+    while (fast?.next != null)
+    {
+      fast = fast.next?.next;
+      (slow.next, reversed, slow) = (reversed, slow, slow.next);
+    }
+    if (fast != null)
+      slow = slow.next;
+    while (reversed != null && slow != null)
+    {
+      if (reversed.val != slow.val)
+        return false;
+      reversed = reversed.next;
+      slow = slow.next;
+    }
+    return true;
+  }
+}
