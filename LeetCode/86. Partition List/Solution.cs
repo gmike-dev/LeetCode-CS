b814eda18fee52a77790e42a481e1a02a7758e60
coffeek <ya.kofeek@yandex.ru>
2023-09-07 23:57:54 +0400

    86. Partition List (extract ListNode.cs)

--- LeetCode/86. Partition List/Solution.cs
+++ LeetCode/86. Partition List/Solution.cs
@@ -1,17 +1,5 @@
 namespace LeetCode._86._Partition_List;
 
-public class ListNode
-{
-  public int val;
-  public ListNode next;
-
-  public ListNode(int val = 0, ListNode next = null)
-  {
-    this.val = val;
-    this.next = next;
-  }
-}
-
 public class Solution
 {
   public ListNode Partition(ListNode head, int x)
