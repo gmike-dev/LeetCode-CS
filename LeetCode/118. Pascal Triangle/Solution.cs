7f250cc774f3948df557e2bb26d4462cf27e0143
coffeek <ya.kofeek@yandex.ru>
2023-09-08 12:41:40 +0400

    118. Pascal's Triangle

---
+++ LeetCode/118. Pascal Triangle/Solution.cs
@@ -0,0 +1,24 @@
+using System.Collections.Generic;
+
+namespace LeetCode._118._Pascal_s_Triangle;
+
+public class Solution
+{
+  public IList<IList<int>> Generate(int numRows)
+  {
+    var result = new int[numRows][];
+    for (var i = 0; i < numRows; i++)
+      result[i] = new int[i + 1];
+    result[0][0] = 1;
+    for (var i = 1; i < numRows; i++)
+    {
+      var prev = result[i - 1];
+      var curr = result[i];
+      curr[0] = 1;
+      for (var j = 1; j < i; j++)
+        curr[j] = prev[j - 1] + prev[j];
+      curr[^1] = 1;
+    }
+    return result;
+  }
+}
---
+++ LeetCode/118. Pascal Triangle/Tests.cs
@@ -0,0 +1,31 @@
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace LeetCode._118._Pascal_s_Triangle;
+
+[TestFixture]
+public class Tests
+{
+  [Test]
+  public void Test1()
+  {
+    new Solution().Generate(6).Should().BeEquivalentTo(new[]
+    {
+      new[] { 1 },
+      new[] { 1, 1 },
+      new[] { 1, 2, 1 },
+      new[] { 1, 3, 3, 1 },
+      new[] { 1, 4, 6, 4, 1 },
+      new[] { 1, 5, 10, 10, 5, 1 }
+    });
+  }
+  
+  [Test]
+  public void Test2()
+  {
+    new Solution().Generate(1).Should().BeEquivalentTo(new[]
+    {
+      new[] { 1 }
+    });
+  }
+}
