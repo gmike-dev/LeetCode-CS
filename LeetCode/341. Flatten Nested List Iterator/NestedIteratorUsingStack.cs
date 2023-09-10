925f18a3866ecffd10948917c75905277ca0bd5d
coffeek <ya.kofeek@yandex.ru>
2023-09-10 16:47:26 +0400

    341. Flatten Nested List Iterator (using stack)

---
+++ LeetCode/341. Flatten Nested List Iterator/NestedIteratorUsingStack.cs
@@ -0,0 +1,42 @@
+using System.Collections.Generic;
+
+namespace LeetCode._341._Flatten_Nested_List_Iterator;
+
+public class NestedIteratorUsingStack
+{
+  private readonly Stack<(IList<NestedInteger> list, int pos)> _states = new();
+  private NestedInteger _currentItem;
+  
+  public NestedIteratorUsingStack(IList<NestedInteger> nestedList)
+  {
+    if (nestedList.Count > 0)
+    {
+      _states.Push((nestedList, 0));
+      _currentItem = FindNextIntItem();
+    }
+  }
+  
+  public bool HasNext() => _currentItem != null;
+  
+  public int Next()
+  {
+    var result = _currentItem.GetInteger();
+    _currentItem = FindNextIntItem();
+    return result;
+  }
+
+  private NestedInteger FindNextIntItem()
+  {
+    while (_states.TryPop(out var state))
+    {
+      if (state.pos >= state.list.Count)
+        continue;
+      _states.Push((state.list, state.pos + 1));
+      var item = state.list[state.pos];
+      if (item.IsInteger())
+        return item;
+      _states.Push((item.GetList(), 0));
+    }
+    return null;
+  }
+}
