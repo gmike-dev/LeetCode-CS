d97adddcfe38204b39544105fab3527b708dda59
Городилов Михаил (Gorodilov_MA) <Gorodilov_MA@directum.ru>
2023-09-11 16:06:42 +0400

    1282. Group the People Given the Group Size They Belong To

---
+++ LeetCode/1282. Group the People/Solution.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+
+namespace LeetCode._1282._Group_the_People;
+
+public class Solution
+{
+  public IList<IList<int>> GroupThePeople(int[] groupSizes)
+  {
+    var n = groupSizes.Length;
+    var peoplesByGroupSize = new List<int>[n + 1];
+    var result = new List<IList<int>>();
+    for (var i = 0; i < n; i++)
+    {
+      var size = groupSizes[i];
+      (peoplesByGroupSize[size] ??= new List<int>(size)).Add(i);
+      if (peoplesByGroupSize[size].Count == size)
+      {
+        result.Add(peoplesByGroupSize[size]);
+        peoplesByGroupSize[size] = null;
+      }
+    }
+    return result;
+  }
+}
---
+++ LeetCode/1282. Group the People/Tests.cs
@@ -0,0 +1,30 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+
+namespace LeetCode._1282._Group_the_People;
+
+[TestFixture]
+public class Tests
+{
+  [Test]
+  public void Test1()
+  {
+    new Solution().GroupThePeople(new[] { 3, 3, 3, 3, 3, 1, 3 }).Should().BeEquivalentTo(new[]
+    {
+      new[] { 5 },
+      new[] { 0, 1, 2 },
+      new[] { 3, 4, 6 },
+    });
+  }
+
+  [Test]
+  public void Test2()
+  {
+    new Solution().GroupThePeople(new[] { 2, 1, 3, 3, 3, 2 }).Should().BeEquivalentTo(new[]
+    {
+      new[] { 1 },
+      new[] { 0, 5 },
+      new[] { 2, 3, 4 },
+    });
+  }
+}
