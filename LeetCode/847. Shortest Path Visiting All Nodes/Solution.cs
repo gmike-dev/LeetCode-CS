8fd6fc5888f796825c067af3f9622b062e6c47c0
coffeek <ya.kofeek@yandex.ru>
2023-09-17 15:22:23 +0400

    847. Shortest Path Visiting All Nodes

---
+++ LeetCode/847. Shortest Path Visiting All Nodes/Solution.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+
+namespace LeetCode._847._Shortest_Path_Visiting_All_Nodes;
+
+public class Solution
+{
+  public int ShortestPathLength(int[][] graph)
+  {
+    var n = graph.Length;
+    var q = new Queue<(int state, int v, int dist)>();
+    var used = new HashSet<(int, int)>();
+    for (var i = 0; i < n; i++)
+      q.Enqueue((1 << i, i, 0));
+    while (q.Count > 0)
+    {
+      var (state, v, dist) = q.Dequeue();
+      if (state == (1 << n) - 1)
+        return dist;
+      for (var i = 0; i < graph[v].Length; i++)
+      {
+        var next = graph[v][i];
+        var nextState = state | (1 << next);
+        if (!used.Contains((nextState, next)))
+        {
+          used.Add((nextState, next));
+          q.Enqueue((nextState, next, dist + 1));
+        }
+      }
+    }
+    return -1;
+  }
+}
---
+++ LeetCode/847. Shortest Path Visiting All Nodes/Tests.cs
@@ -0,0 +1,26 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+
+namespace LeetCode._847._Shortest_Path_Visiting_All_Nodes;
+
+[TestFixture]
+public class Tests
+{
+  [Test]
+  public void Test1()
+  {
+    new Solution().ShortestPathLength(new[]
+    {
+      new[] { 1, 2, 3 }, new[] { 0 }, new[] { 0 }, new[] { 0 }
+    }).Should().Be(4);
+  }
+
+  [Test]
+  public void Test2()
+  {
+    new Solution().ShortestPathLength(new[]
+    {
+      new[] { 1 }, new[] { 0, 2, 4 }, new[] { 1, 3, 4 }, new[] { 2 }, new[] { 1, 2 }
+    }).Should().Be(4);
+  }
+}
