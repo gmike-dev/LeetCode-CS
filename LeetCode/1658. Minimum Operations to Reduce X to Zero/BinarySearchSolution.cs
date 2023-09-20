f88b80b2f47543880bdb902cc852ec08f746742a
coffeek <ya.kofeek@yandex.ru>
2023-09-20 22:22:49 +0400

    1658. Minimum Operations to Reduce X to Zero (binary search solution)

---
+++ LeetCode/1658. Minimum Operations to Reduce X to Zero/BinarySearchSolution.cs
@@ -0,0 +1,28 @@
+using System;
+
+namespace LeetCode._1658._Minimum_Operations_to_Reduce_X_to_Zero;
+
+public class BinarySearchSolution
+{
+  public int MinOperations(int[] nums, int x)
+  {
+    var n = nums.Length;
+    var sl = new int[n + 1];
+    for (var i = 0; i < n; i++)
+      sl[i + 1] = sl[i] + nums[i];
+    if (sl[^1] < x)
+      return -1;
+    if (sl[^1] == x)
+      return n;
+    var ans = int.MaxValue;
+    var sr = 0;
+    for (var r = 0; r < n && sr <= x; r++, sr += nums[n - r])
+    {
+      var l = sl.AsSpan(0, n - r).BinarySearch(x - sr);
+      if (l >= 0)
+        ans = Math.Min(ans, l + r);
+    }
+    return ans == int.MaxValue ? -1 : ans;
+  }
+
+}
