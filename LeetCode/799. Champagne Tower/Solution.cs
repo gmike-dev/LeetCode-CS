53f42241412a3039ae220d577df039776ee95578
coffeek <ya.kofeek@yandex.ru>
2023-09-24 22:18:35 +0400

    799. Champagne Tower

---
+++ LeetCode/799. Champagne Tower/Solution.cs
@@ -0,0 +1,27 @@
+using System;
+
+namespace LeetCode._799._Champagne_Tower;
+
+public class Solution
+{
+  public double ChampagneTower(int poured, int queryRow, int queryGlass)
+  {
+    var prev = new double[queryRow + 1];
+    var next = new double[queryRow + 1];
+    prev[0] = poured;
+    for (var i = 1; i <= queryRow; i++)
+    {
+      next.AsSpan().Clear();
+      for (var j = 0; j < i; j++)
+      {
+        if (prev[j] > 1)
+        {
+          next[j] += (prev[j] - 1) / 2.0;
+          next[j + 1] += (prev[j] - 1) / 2.0;
+        }
+      }
+      (prev, next) = (next, prev);
+    }
+    return Math.Min(1.0, prev[queryGlass]);
+  }
+}
---
+++ LeetCode/799. Champagne Tower/Tests.cs
@@ -0,0 +1,19 @@
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace LeetCode._799._Champagne_Tower;
+
+[TestFixture]
+public class Tests
+{
+  [TestCase(1, 1, 1, 0.0)]
+  [TestCase(2, 1, 1, 0.5)]
+  [TestCase(100000009, 33, 17, 1.0)]
+  [TestCase(9, 3, 0, 0.25)]
+  [TestCase(9, 3, 1, 1.0)]
+  [TestCase(9, 4, 1, 0.125)]
+  public void Test(int poured, int queryRow, int queryGlass, double expected)
+  {
+    new Solution().ChampagneTower(poured, queryRow, queryGlass).Should().Be(expected);
+  }
+}
