ac7c6526cab37126355d969d6234ac9bc4c1c081
coffeek <ya.kofeek@yandex.ru>
2023-10-08 01:17:49 +0400

    2245. Maximum Trailing Zeros in a Cornered Path (refactoring)

--- LeetCode/2245. Maximum Trailing Zeros/SolutionUsingStruct.cs
+++ LeetCode/2245. Maximum Trailing Zeros/SolutionUsingStruct.cs
@@ -5,49 +5,16 @@ namespace LeetCode._2245._Maximum_Trailing_Zeros;
 
 public class SolutionUsingStruct
 {
-  private struct Zeroes
-  {
-    private readonly int _twos;
-    private readonly int _fives;
-
-    private static int NumCount(int n, int x)
-    {
-      var count = 0;
-      while (n % x == 0)
-      {
-        count++;
-        n /= x;
-      }
-      return count;
-    }
-
-    public static Zeroes operator +(Zeroes x, Zeroes y) => new(x._twos + y._twos, x._fives + y._fives);
-    public static Zeroes operator -(Zeroes x, Zeroes y) => new(x._twos - y._twos, x._fives - y._fives);
-
-    public static implicit operator int(Zeroes x) => Math.Min(x._twos, x._fives);
-
-    private Zeroes(int twos, int fives)
-    {
-      _twos = twos;
-      _fives = fives;
-    }
-
-    public Zeroes(int value)
-      : this(NumCount(value, 2), NumCount(value, 5))
-    {
-    }
-  }
-
   public int MaxTrailingZeros(int[][] grid)
   {
     var m = grid.Length;
     var n = grid[0].Length;
-    var z = new Zeroes[m, n];
+    var z = new Zeros[m, n];
     for (var i = 0; i < m; i++)
     for (var j = 0; j < n; j++)
-      z[i, j] = new Zeroes(grid[i][j]);
-    var rowSum = new Zeroes[m, n];
-    var colSum = new Zeroes[m, n];
+      z[i, j] = new Zeros(grid[i][j]);
+    var rowSum = new Zeros[m, n];
+    var colSum = new Zeros[m, n];
     for (var i = 0; i < m; i++)
     {
       rowSum[i, 0] = z[i, 0];
@@ -74,4 +41,37 @@ public class SolutionUsingStruct
   }
 
   private static int Max(params int[] values) => values.Max();
+
+  private struct Zeros
+  {
+    private readonly int _twos;
+    private readonly int _fives;
+
+    private static int NumCount(int n, int x)
+    {
+      var count = 0;
+      while (n % x == 0)
+      {
+        count++;
+        n /= x;
+      }
+      return count;
+    }
+
+    public static Zeros operator +(Zeros x, Zeros y) => new(x._twos + y._twos, x._fives + y._fives);
+    public static Zeros operator -(Zeros x, Zeros y) => new(x._twos - y._twos, x._fives - y._fives);
+
+    public static implicit operator int(Zeros x) => Math.Min(x._twos, x._fives);
+
+    private Zeros(int twos, int fives)
+    {
+      _twos = twos;
+      _fives = fives;
+    }
+
+    public Zeros(int value)
+      : this(NumCount(value, 2), NumCount(value, 5))
+    {
+    }
+  }
 }
