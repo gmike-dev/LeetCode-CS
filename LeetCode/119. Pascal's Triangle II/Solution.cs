6b0aaaf4aa58996968b95a9355f6ed4123ee5881
Городилов Михаил (Gorodilov_MA) <Gorodilov_MA@directum.ru>
2023-10-16 10:17:22 +0400

    119. Pascal's Triangle II

---
+++ LeetCode/119. Pascal's Triangle II/Solution.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+
+namespace LeetCode._119._Pascal_s_Triangle_II;
+
+public class Solution
+{
+  public IList<int> GetRow(int rowIndex)
+  {
+    var a = new int[rowIndex + 1];
+    for (var i = 0; i <= rowIndex; i++)
+    {
+      a[0] = 1;
+      var prev = a[0];
+      for (var j = 1; j <= i; j++)
+      {
+        (a[j], prev) = (prev + a[j], a[j]);
+      }
+    }
+    return a;
+  }
+}
---
+++ LeetCode/119. Pascal's Triangle II/Tests.cs
@@ -0,0 +1,19 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+
+namespace LeetCode._119._Pascal_s_Triangle_II;
+
+[TestFixture]
+public class Tests
+{
+  [TestCase(0, new[] { 1 })]
+  [TestCase(1, new[] { 1, 1 })]
+  [TestCase(2, new[] { 1, 2, 1 })]
+  [TestCase(3, new[] { 1, 3, 3, 1 })]
+  [TestCase(4, new[] { 1, 4, 6, 4, 1 })]
+  [TestCase(5, new[] { 1, 5, 10, 10, 5, 1 })]
+  public void Test(int rowIndex, int[] expected)
+  {
+    new Solution().GetRow(rowIndex).Should().BeEquivalentTo(expected, o => o.WithStrictOrdering());
+  }
+}
