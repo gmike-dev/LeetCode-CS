cf2d0d49f9ea3a795258a4e38a2a03c05596bcf0
coffeek <ya.kofeek@yandex.ru>
2023-10-19 11:40:04 +0400

    844. Backspace String Compare

---
+++ LeetCode/844. Backspace String Compare/BuildStringSolution.cs
@@ -0,0 +1,30 @@
+using System;
+
+namespace LeetCode._844._Backspace_String_Compare;
+
+public class BuildStringSolution
+{
+  public bool BackspaceCompare(string s, string t)
+  {
+    return Print(s).SequenceEqual(Print(t));
+  }
+
+  private static ReadOnlySpan<char> Print(string s)
+  {
+    var a = new char[s.Length];
+    var i = 0;
+    foreach (var c in s)
+    {
+      if (c == '#')
+      {
+        i = Math.Max(0, i - 1);
+      }
+      else
+      {
+        a[i] = c;
+        i++;
+      }
+    }
+    return a.AsSpan(0, i);
+  }
+}
---
+++ LeetCode/844. Backspace String Compare/Tests.cs
@@ -0,0 +1,17 @@
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace LeetCode._844._Backspace_String_Compare;
+
+[TestFixture]
+public class Tests
+{
+  [TestCase("ab#c", "ad#c", true)]
+  [TestCase("ab##", "c#d#", true)]
+  [TestCase("a#c", "b", false)]
+  [TestCase("##", "#", true)]
+  public void Test(string s, string t, bool expected)
+  {
+    new BuildStringSolution().BackspaceCompare(s, t).Should().Be(expected);
+  }
+}
