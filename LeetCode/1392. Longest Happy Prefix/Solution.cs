3125ea138c7728012bc046fa612caf9c724f87bc
coffeek <ya.kofeek@yandex.ru>
2023-10-21 21:30:54 +0400

    1392. Longest Happy Prefix

---
+++ LeetCode/1392. Longest Happy Prefix/Solution.cs
@@ -0,0 +1,26 @@
+﻿namespace LeetCode._1392._Longest_Happy_Prefix;
+
+public class Solution
+{
+  public string LongestPrefix(string s)
+  {
+    var pi = Pi(s);
+    return s[..pi[^1]];
+  }
+
+  private static int[] Pi(string s)
+  {
+    var n = s.Length;
+    var pi = new int[n];
+    for (var i = 1; i < n; i++)
+    {
+      var j = pi[i - 1];
+      while (j != 0 && s[j] != s[i])
+        j = pi[j - 1];
+      if (s[j] == s[i])
+        j++;
+      pi[i] = j;
+    }
+    return pi;
+  }
+}
---
+++ LeetCode/1392. Longest Happy Prefix/Tests.cs
@@ -0,0 +1,17 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+
+namespace LeetCode._1392._Longest_Happy_Prefix;
+
+[TestFixture]
+public class Tests
+{
+  [TestCase("level", "l")]
+  [TestCase("ababab", "abab")]
+  [TestCase("bba", "")]
+  [TestCase("aba", "a")]
+  public void Test(string s, string expected)
+  {
+    new Solution().LongestPrefix(s).Should().Be(expected);
+  }
+}
