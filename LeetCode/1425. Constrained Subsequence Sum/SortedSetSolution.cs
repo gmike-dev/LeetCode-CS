09e7db5ae1ead9277056b9c9341430765388ca15
coffeek <ya.kofeek@yandex.ru>
2023-10-21 19:18:03 +0400

    1425. Constrained Subsequence Sum

---
+++ LeetCode/1425. Constrained Subsequence Sum/SortedSetSolution.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LeetCode._1425._Constrained_Subsequence_Sum;
+
+public class SortedSetSolution
+{
+  public int ConstrainedSubsetSum(int[] nums, int k)
+  {
+    var q = new SortedSet<(int sum, int i)> { (nums[0], 0) };
+    var s = new int[nums.Length];
+    s[0] = nums[0];
+    for (var i = 1; i < nums.Length; i++)
+    {
+      s[i] = Math.Max(nums[i], q.Max.sum + nums[i]);
+      q.Add((s[i], i));
+      if (i >= k)
+        q.Remove((s[i - k], i - k));
+    }
+    return s.Max();
+  }
+}
