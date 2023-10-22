c618ed4e8ed709b8d6a15098442575b6c6e8be73
coffeek <ya.kofeek@yandex.ru>
2023-10-22 14:40:04 +0400

    1793. Maximum Score of a Good Subarray

---
+++ LeetCode/1793. Maximum Score of a Good Subarray/Solution.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace LeetCode._1793._Maximum_Score_of_a_Good_Subarray;
+
+public class Solution
+{
+  public int MaximumScore(int[] a, int k)
+  {
+    var n = a.Length;
+    var l = k;
+    var r = k;
+    var ans = a[k];
+    var min = a[k];
+    while (l >= 0 || r < n)
+    {
+      if (l == 0)
+      {
+        for (r++; r < n; r++)
+        {
+          min = Math.Min(min, a[r]);
+          ans = Math.Max(ans, (r - l + 1) * min);
+        }
+        break;
+      }
+      if (r == n - 1)
+      {
+        for (l--; l >= 0; l--)
+        {
+          min = Math.Min(min, a[l]);
+          ans = Math.Max(ans, (r - l + 1) * min);
+        }
+        break;
+      }
+      if (a[l - 1] >= a[r + 1])
+      {
+        l--;
+        min = Math.Min(min, a[l]);
+      }
+      else if (a[l - 1] < a[r + 1])
+      {
+        r++;
+        min = Math.Min(min, a[r]);
+      }
+      ans = Math.Max(ans, (r - l + 1) * min);
+    }
+    return ans;
+  }
+}
---
+++ LeetCode/1793. Maximum Score of a Good Subarray/Tests.cs
@@ -0,0 +1,18 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+
+namespace LeetCode._1793._Maximum_Score_of_a_Good_Subarray;
+
+[TestFixture]
+public class Tests
+{
+  [TestCase(new[] { 1, 4, 3, 7, 4, 5 }, 3, 15)]
+  [TestCase(new[] { 5, 5, 4, 5, 4, 1, 1, 1 }, 0, 20)]
+  [TestCase(new[] { 5, 5, 4, 5, 4, 1, 1, 1 }, 7, 8)]
+  [TestCase(new[] { 5, 5, 4, 5, 4, 1, 1, 1 }, 5, 8)]
+  [TestCase(new[] { 5, 5, 4, 5, 4, 1, 1, 1 }, 4, 20)]
+  public void Test(int[] nums, int k, int expected)
+  {
+    new Solution().MaximumScore(nums, k).Should().Be(expected);
+  }
+}
