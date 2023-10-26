a31503f99f9a0a9ebf2ac00284ffe104d2ec2b08
coffeek <ya.kofeek@yandex.ru>
2023-10-26 22:14:41 +0400

    823. Binary Trees With Factors

---
+++ LeetCode/823. Binary Trees With Factors/DpSolution.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Linq;
+
+namespace LeetCode._823._Binary_Trees_With_Factors;
+
+public class DpSolution
+{
+  public int NumFactoredBinaryTrees(int[] arr)
+  {
+    const int mod = 1000000007;
+    Array.Sort(arr);
+    var dp = arr.ToDictionary(x => x, _ => 1);
+    var ans = 0;
+    for (var i = 0; i < arr.Length; i++)
+    {
+      var a = arr[i];
+      for (var j = 0; j < i; j++)
+      {
+        var b = arr[j];
+        if (a % b == 0 && dp.TryGetValue(a / b, out var ab))
+          dp[a] = (int)((dp[a] + (long)dp[b] * ab) % mod);
+      }
+      ans = (ans + dp[a]) % mod;
+    }
+    return ans;
+  }
+}
---
+++ LeetCode/823. Binary Trees With Factors/DpWithMemoizationSolution.cs
@@ -0,0 +1,41 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LeetCode._823._Binary_Trees_With_Factors;
+
+public class DpWithMemoizationSolution
+{
+  public int NumFactoredBinaryTrees(int[] arr)
+  {
+    Array.Sort(arr);
+    const int mod = 1000000007;
+    var nums = arr.ToHashSet();
+    var dp = new Dictionary<int, int>();
+    var ans = 0;
+    for (var i = arr.Length - 1; i >= 0; i--)
+      ans = (ans + F(arr[i])) % mod;
+    return ans;
+
+    int F(int k)
+    {
+      if (dp.TryGetValue(k, out var count))
+        return count;
+      count = 1;
+      for (var i = 0; i < arr.Length && arr[i] * arr[i] <= k; i++)
+      {
+        var a = arr[i];
+        if (k % a != 0)
+          continue;
+        var b = k / arr[i];
+        if (a == b || nums.Contains(b))
+        {
+          var mul = a == b ? 1L : 2L;
+          count = (int)((count + mul * F(a) * F(b)) % mod);
+        }
+      }
+      dp[k] = count;
+      return count;
+    }
+  }
+}
