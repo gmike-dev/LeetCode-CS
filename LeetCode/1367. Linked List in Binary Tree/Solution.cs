db0fdaa3411dda9560136abf1c22360182a4e5b0
coffeek <ya.kofeek@yandex.ru>
2023-10-31 23:41:10 +0400

    1367. Linked List in Binary Tree

---
+++ LeetCode/1367. Linked List in Binary Tree/Solution.cs
@@ -0,0 +1,31 @@
+namespace LeetCode._1367._Linked_List_in_Binary_Tree;
+
+public class Solution
+{
+  public bool IsSubPath(ListNode head, TreeNode root)
+  {
+    if (TestPath(head, root))
+      return true;
+    
+    var result = false;
+    if (root.left != null)
+      result = IsSubPath(head, root.left);
+    if (!result && root.right != null)
+      result = IsSubPath(head, root.right);
+    return result;
+  }
+  
+  private static bool TestPath(ListNode head, TreeNode root)
+  {
+    if (head.val != root.val)
+      return false;
+    if (head.next == null)
+      return true;
+    var result = false;
+    if (root.left != null)
+      result = TestPath(head.next, root.left);
+    if (!result && root.right != null)
+      result = TestPath(head.next, root.right);
+    return result;
+  }
+}
