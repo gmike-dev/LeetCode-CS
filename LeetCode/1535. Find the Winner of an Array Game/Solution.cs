fa9af289ba2aa6c893a95f6de09541d40515f22d
coffeek <ya.kofeek@yandex.ru>
2023-11-05 22:05:30 +0400

    1535. Find the Winner of an Array Game

---
+++ LeetCode/1535. Find the Winner of an Array Game/Solution.cs
@@ -0,0 +1,24 @@
+﻿namespace LeetCode._1535._Find_the_Winner_of_an_Array_Game;
+
+public class Solution
+{
+  public int GetWinner(int[] a, int k)
+  {
+    var n = a.Length;
+    var x = a[0];
+    var wins = 0;
+    for (var i = 1; i < n && wins < k; i++)
+    {
+      if (x > a[i])
+      {
+        wins++;
+      }
+      else
+      {
+        x = a[i];
+        wins = 1;
+      }
+    }
+    return x;
+  }
+}
---
+++ LeetCode/1535. Find the Winner of an Array Game/Tests.cs
@@ -0,0 +1,15 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+
+namespace LeetCode._1535._Find_the_Winner_of_an_Array_Game;
+
+[TestFixture]
+public class Tests
+{
+  [TestCase(new[] { 2, 1, 3, 5, 4, 6, 7 }, 2, 5)]
+  [TestCase(new[] { 3, 2, 1 }, 10, 3)]
+  public void Test(int[] nums, int k, int expected)
+  {
+    new Solution().GetWinner(nums, k).Should().Be(expected);
+  }
+}
