226e9ae6ad7f72c16f1c0a1594e3971a0d2df71c
coffeek <ya.kofeek@yandex.ru>
2023-11-06 11:21:25 +0400

    1845. Seat Reservation Manager

---
+++ LeetCode/1845. Seat Reservation Manager/SeatManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace LeetCode._1845._Seat_Reservation_Manager;
+
+public class SeatManager
+{
+  readonly SortedSet<int> unreserved = new();
+  private int next = 1;
+
+  public SeatManager(int n)
+  {
+  }
+
+  public int Reserve()
+  {
+    if (unreserved.Count == 0)
+      return next++;
+
+    var result = unreserved.Min;
+    unreserved.Remove(result);
+    return result;
+  }
+
+  public void Unreserve(int seatNumber)
+  {
+    unreserved.Add(seatNumber);
+  }
+}
---
+++ LeetCode/1845. Seat Reservation Manager/Tests.cs
@@ -0,0 +1,23 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+
+namespace LeetCode._1845._Seat_Reservation_Manager;
+
+[TestFixture]
+public class Tests
+{
+  [Test]
+  public void Test()
+  {
+    var seatManager = new SeatManager(5);
+    seatManager.Reserve().Should().Be(1);
+    seatManager.Reserve().Should().Be(2);
+    seatManager.Unreserve(2);
+    seatManager.Reserve().Should().Be(2);
+    seatManager.Reserve().Should().Be(3);
+    seatManager.Reserve().Should().Be(4);
+    seatManager.Reserve().Should().Be(5);
+    seatManager.Unreserve(5);
+    seatManager.Reserve().Should().Be(5);
+  }
+}
