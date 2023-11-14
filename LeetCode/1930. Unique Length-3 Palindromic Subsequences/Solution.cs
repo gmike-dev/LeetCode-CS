c42019add5537070819c85ebe0592a0d6676dc3c
coffeek <ya.kofeek@yandex.ru>
2023-11-14 11:11:28 +0400

    1930. Unique Length-3 Palindromic Subsequences

---
+++ LeetCode/1930. Unique Length-3 Palindromic Subsequences/Solution.cs
@@ -0,0 +1,27 @@
+using System.Linq;
+using System.Numerics;
+
+namespace LeetCode._1930._Unique_Length_3_Palindromic_Subsequences;
+
+public class Solution
+{
+  public int CountPalindromicSubsequence(string s)
+  {
+    var left = new int[26];
+    var right = new int[26];
+    left[s[0] - 'a'] = 1;
+    for (var i = 1; i < s.Length; i++)
+      right[s[i] - 'a']++;
+    var counter = new int[26];
+    for (var i = 1; i < s.Length - 1; i++)
+    {
+      var c = s[i] - 'a';
+      right[c]--;
+      for (var j = 0; j < 26; j++)
+        if (left[j] > 0 && right[j] > 0)
+          counter[c] |= 1 << j;
+      left[c]++;
+    }
+    return counter.Sum(x => BitOperations.PopCount((uint)x));
+  }
+}
---
+++ LeetCode/1930. Unique Length-3 Palindromic Subsequences/Tests.cs
@@ -0,0 +1,16 @@
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace LeetCode._1930._Unique_Length_3_Palindromic_Subsequences;
+
+[TestFixture]
+public class Tests
+{
+  [TestCase("aabca", 3)]
+  [TestCase("acd", 0)]
+  [TestCase("bbcbaba", 4)]
+  public void Test(string s, int expected)
+  {
+    new Solution().CountPalindromicSubsequence(s).Should().Be(expected);
+  }
+}
