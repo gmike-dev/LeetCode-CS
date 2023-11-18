c9f68dcfb261a74934d9eac566c3cd3483d4f68d
coffeek <ya.kofeek@yandex.ru>
2023-11-18 20:58:20 +0400

    1838. Frequency of the Most Frequent

---
+++ LeetCode/1838. Frequency of the Most Frequent/Solution.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace LeetCode._1838._Frequency_of_the_Most_Frequent;
+
+public class Solution
+{
+  public int MaxFrequency(int[] nums, int k)
+  {
+    Array.Sort(nums);
+    var count = 1;
+    int j = 0;
+    var sum = 0;
+    for (var i = 1; i < nums.Length; i++)
+    {
+      sum += (nums[i] - nums[i - 1]) * (i - j);
+      while (sum > k)
+      {
+        sum -= nums[i] - nums[j];
+        j++;
+      }
+      count = Math.Max(count, i - j + 1);
+    }
+    return count;
+  }
+}
