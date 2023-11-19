82f18f2f4e4de68855a9629e385097c2b7a7437f
coffeek <ya.kofeek@yandex.ru>
2023-11-19 09:43:00 +0400

    1887. Reduction Operations to Make the Array Elements Equal

---
+++ LeetCode/1887. Reduction Operations/Solution.cs
@@ -0,0 +1,24 @@
+﻿namespace LeetCode._1887._Reduction_Operations;
+
+public class Solution
+{
+  public int ReductionOperations(int[] nums)
+  {
+    var cnt = new int[50001];
+    foreach (var num in nums)
+    {
+      cnt[num]++;
+    }
+    var prev = 0;
+    var s = 0;
+    for (var i = cnt.Length - 1; i > 0; i--)
+    {
+      if (cnt[i] > 0)
+      {
+        s += prev;
+        prev += cnt[i];
+      }
+    }
+    return s;
+  }
+}
---
+++ LeetCode/1887. Reduction Operations/Tests.cs
@@ -0,0 +1,16 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+
+namespace LeetCode._1887._Reduction_Operations;
+
+[TestFixture]
+public class Tests
+{
+  [TestCase(new[] { 5, 1, 3 }, 3)]
+  [TestCase(new[] { 1, 1, 1 }, 0)]
+  [TestCase(new[] { 1, 1, 2, 2, 3 }, 4)]
+  public void Test(int[] nums, int expected)
+  {
+    new Solution().ReductionOperations(nums).Should().Be(expected);
+  }
+}
