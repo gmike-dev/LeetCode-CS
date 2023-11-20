c8578160803f2f85e40edf61d2b715b2784127f0
Городилов Михаил (Gorodilov_MA) <Gorodilov_MA@directum.ru>
2023-11-20 18:20:59 +0400

    2391. Minimum Amount of Time to Collect Garbage

---
+++ LeetCode/2391. Minimum Amount of Time to Collect Garbage/Solution.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections.Generic;
+
+namespace LeetCode._2391._Minimum_Amount_of_Time_to_Collect_Garbage;
+
+public class Solution
+{
+  public int GarbageCollection(string[] garbage, int[] travel)
+  {
+    var n = garbage.Length;
+    var count = new Dictionary<char, int[]>
+    {
+      ['M'] = new int[n],
+      ['P'] = new int[n],
+      ['G'] = new int[n]
+    };
+    var last = new Dictionary<char, int>();
+    for (var i = 0; i < n; i++)
+    {
+      for (var j = 0; j < garbage[i].Length; j++)
+      {
+        var g = garbage[i][j];
+        count[g][i]++;
+        last[g] = i;
+      }
+    }
+
+    var total = 0;
+    foreach (var g in "MPG")
+    {
+      if (last.TryGetValue(g, out var m))
+      {
+        var cnt = count[g];
+        for (var i = 0; i < m; i++)
+        {
+          total += cnt[i];
+          total += travel[i];
+        }
+        total += count[g][m];
+      }
+    }
+    return total;
+  }
+}
---
+++ LeetCode/2391. Minimum Amount of Time to Collect Garbage/Tests.cs
@@ -0,0 +1,15 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+
+namespace LeetCode._2391._Minimum_Amount_of_Time_to_Collect_Garbage;
+
+[TestFixture]
+public class Tests
+{
+  [TestCase(new[] { "G", "P", "GP", "GG" }, new[] { 2, 4, 3 }, 21)]
+  [TestCase(new[] { "MMM", "PGM", "GP" }, new[] { 3, 10 }, 37)]
+  public void Test(string[] garbage, int[] travel, int expected)
+  {
+    new Solution().GarbageCollection(garbage, travel).Should().Be(expected);
+  }
+}
