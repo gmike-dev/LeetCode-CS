fd8f985bc732aeb6e94df46760c2fcec1b1caa05
coffeek <ya.kofeek@yandex.ru>
2023-11-22 12:34:58 +0400

    1424. Diagonal Traverse II (refactoring)

--- LeetCode/1424. Diagonal Traverse II/SortingSolution.cs
+++ LeetCode/1424. Diagonal Traverse II/SortingSolution.cs
@@ -7,11 +7,11 @@ public class SortingSolution
 {
   public int[] FindDiagonalOrder(IList<IList<int>> nums)
   {
-    var list = new List<(int d, int pos, int value)>();
+    var items = new List<(int diagonal, int row, int value)>();
     for (var i = 0; i < nums.Count; i++)
     for (var j = 0; j < nums[i].Count; j++)
-      list.Add((i + j, -i, nums[i][j]));
-    list.Sort();
-    return list.Select(v => v.value).ToArray();
+      items.Add((i + j, -i, nums[i][j]));
+    items.Sort();
+    return items.Select(item => item.value).ToArray();
   }
 }
