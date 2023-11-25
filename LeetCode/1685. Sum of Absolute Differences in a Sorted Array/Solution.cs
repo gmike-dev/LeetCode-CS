dec9d8ce80a95a7b1cc2895a57a83f2ac919de61
coffeek <ya.kofeek@yandex.ru>
2023-11-25 11:17:08 +0400

    1685. Sum of Absolute Differences in a Sorted Array

---
+++ LeetCode/1685. Sum of Absolute Differences in a Sorted Array/Solution.cs
@@ -0,0 +1,21 @@
+using System.Linq;
+
+namespace LeetCode._1685._Sum_of_Absolute_Differences_in_a_Sorted_Array;
+
+public class Solution
+{
+  public int[] GetSumAbsoluteDifferences(int[] nums)
+  {
+    var n = nums.Length;
+    var sr = nums.Sum();
+    var sl = 0;
+    for (var i = 0; i < n; i++)
+    {
+      var val = nums[i];
+      nums[i] = (2 * i - n) * val + sr - sl;
+      sr -= val;
+      sl += val;
+    }
+    return nums;
+  }
+}
---
+++ LeetCode/1685. Sum of Absolute Differences in a Sorted Array/Tests.cs
@@ -0,0 +1,16 @@
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace LeetCode._1685._Sum_of_Absolute_Differences_in_a_Sorted_Array;
+
+[TestFixture]
+public class Tests
+{
+  [TestCase(new[] { 2, 3, 5 }, new[] { 4, 3, 5 })]
+  [TestCase(new[] { 1, 4, 6, 8, 10 }, new[] { 24, 15, 13, 15, 21 })]
+  [TestCase(new[] { 1, 20 }, new[] { 19, 19 })]
+  public void Test(int[] nums, int[] expected)
+  {
+    new Solution().GetSumAbsoluteDifferences(nums).Should().BeEquivalentTo(expected, o => o.WithStrictOrdering());
+  }
+}
