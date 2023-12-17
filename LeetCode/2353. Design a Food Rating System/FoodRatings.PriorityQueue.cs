7b693ee502be5f02a50ef3a4852aa0ef4ad6f37b
coffeek <ya.kofeek@yandex.ru>
2023-12-17 19:04:02 +0400

    2353. Design a Food Rating System (PriorityQueue solution)

---
+++ LeetCode/2353. Design a Food Rating System/FoodRatings.PriorityQueue.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace LeetCode._2353._Design_a_Food_Rating_System.PriorityQueue;
+
+public class FoodRatings
+{
+  private readonly Dictionary<string, string> foodCuisine = new();
+  private readonly Dictionary<string, int> foodRating = new();
+  private readonly Dictionary<string, PriorityQueue<string, (int rating, string food)>> cuisineFoodRatings = new();
+  
+  public FoodRatings(string[] foods, string[] cuisines, int[] ratings)
+  {
+    foreach (var (food, cuisine, rating) in foods.Zip(cuisines, ratings))
+    {
+      foodCuisine[food] = cuisine;
+      foodRating[food] = rating;
+      if (!cuisineFoodRatings.TryGetValue(cuisine, out var bestRatings))
+        cuisineFoodRatings[cuisine] = bestRatings = new();
+      bestRatings.Enqueue(food, (-rating, food));
+    }
+  }
+
+  public void ChangeRating(string food, int newRating)
+  {
+    foodRating[food] = newRating;
+    cuisineFoodRatings[foodCuisine[food]].Enqueue(food, (-newRating, food));
+  }
+
+  public string HighestRated(string cuisine)
+  {
+    var cuisineFoodRating = cuisineFoodRatings[cuisine];
+    while (cuisineFoodRating.TryPeek(out var food, out var priority) && -priority.rating != foodRating[food])
+      cuisineFoodRating.Dequeue();
+    return cuisineFoodRating.Peek();
+  }
+}
--- LeetCode/2353. Design a Food Rating System/FoodRatingsTests.cs
+++ LeetCode/2353. Design a Food Rating System/FoodRatingsTests.cs
@@ -7,9 +7,29 @@ namespace LeetCode._2353._Design_a_Food_Rating_System;
 public class FoodRatingsTests
 {
   [Test]
-  public void Test1()
+  public void SortedSetTest()
   {
-    var foodRatings = new FoodRatings(
+    var foodRatings = new SortedSet.FoodRatings(
+      new[] { "kimchi", "miso", "sushi", "moussaka", "ramen", "bulgogi" },
+      new[] { "korean", "japanese", "japanese", "greek", "japanese", "korean" },
+      new[] { 9, 12, 8, 15, 14, 7 });
+    foodRatings.HighestRated("korean").Should().Be("kimchi"); // return "kimchi"
+    // "kimchi" is the highest rated korean food with a rating of 9.
+    foodRatings.HighestRated("japanese").Should().Be("ramen"); // return "ramen"
+    // "ramen" is the highest rated japanese food with a rating of 14.
+    foodRatings.ChangeRating("sushi", 16); // "sushi" now has a rating of 16.
+    foodRatings.HighestRated("japanese").Should().Be("sushi"); // return "sushi"
+    // "sushi" is the highest rated japanese food with a rating of 16.
+    foodRatings.ChangeRating("ramen", 16); // "ramen" now has a rating of 16.
+    foodRatings.HighestRated("japanese").Should().Be("ramen"); // return "ramen"
+    // Both "sushi" and "ramen" have a rating of 16.
+    // However, "ramen" is lexicographically smaller than "sushi".
+  }
+  
+  [Test]
+  public void PriorityQueueTest()
+  {
+    var foodRatings = new PriorityQueue.FoodRatings(
       new[] { "kimchi", "miso", "sushi", "moussaka", "ramen", "bulgogi" },
       new[] { "korean", "japanese", "japanese", "greek", "japanese", "korean" },
       new[] { 9, 12, 8, 15, 14, 7 });
