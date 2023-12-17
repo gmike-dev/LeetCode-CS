65bb4b4a5663a09faec3efbd7e97cbf2a41e7f9f
coffeek <ya.kofeek@yandex.ru>
2023-12-17 14:55:33 +0400

    2353. Design a Food Rating System

---
+++ LeetCode/2353. Design a Food Rating System/FoodRatingsTests.cs
@@ -0,0 +1,28 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+
+namespace LeetCode._2353._Design_a_Food_Rating_System;
+
+[TestFixture]
+public class FoodRatingsTests
+{
+  [Test]
+  public void Test1()
+  {
+    var foodRatings = new FoodRatings(
+      new[] { "kimchi", "miso", "sushi", "moussaka", "ramen", "bulgogi" },
+      new[] { "korean", "japanese", "japanese", "greek", "japanese", "korean" },
+      new[] { 9, 12, 8, 15, 14, 7 });
+    foodRatings.HighestRated("korean").Should().Be("kimchi"); // return "kimchi"
+    // "kimchi" is the highest rated korean food with a rating of 9.
+    foodRatings.HighestRated("japanese").Should().Be("ramen"); // return "ramen"
+    // "ramen" is the highest rated japanese food with a rating of 14.
+    foodRatings.ChangeRating("sushi", 16); // "sushi" now has a rating of 16.
+    foodRatings.HighestRated("japanese").Should().Be("sushi"); // return "sushi"
+    // "sushi" is the highest rated japanese food with a rating of 16.
+    foodRatings.ChangeRating("ramen", 16); // "ramen" now has a rating of 16.
+    foodRatings.HighestRated("japanese").Should().Be("ramen"); // return "ramen"
+    // Both "sushi" and "ramen" have a rating of 16.
+    // However, "ramen" is lexicographically smaller than "sushi".
+  }
+}
