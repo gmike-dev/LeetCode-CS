a6cc732a9e7b75db8cdd82fb0d3dfc9c1c0b9471
coffeek <ya.kofeek@yandex.ru>
2023-12-29 00:46:00 +0400

    1531. String Compression II

---
+++ LeetCode/1531. String Compression II/Solution.cs
@@ -0,0 +1,54 @@
+using System;
+
+namespace LeetCode._1531._String_Compression_II;
+
+public class Solution
+{
+  private int[][] dp;
+
+  public int GetLengthOfOptimalCompression(string s, int k)
+  {
+    dp = new int[s.Length][];
+    for (var i = 0; i < s.Length; i++)
+    {
+      dp[i] = new int[k + 1];
+      Array.Fill(dp[i], -1);
+    }
+    return F(s, 0, k);
+  }
+
+  private int F(string s, int start, int k)
+  {
+    var n = s.Length;
+    if (k < 0)
+      return n;
+    if (start >= n || k >= n - start)
+      return 0;
+    if (dp[start][k] != -1)
+      return dp[start][k];
+    var cnt = new int['z' - 'a' + 1];
+    var freq = 0;
+    var result = int.MaxValue;
+    for (var i = start; i < n; i++)
+    {
+      var c = s[i] - 'a';
+      cnt[c]++;
+      freq = Math.Max(freq, cnt[c]);
+      var length = Length(freq) + F(s, i + 1, k - (i - start + 1 - freq));
+      result = Math.Min(result, length);
+    }
+    return dp[start][k] = result;
+  }
+
+  private static int Length(int charCount)
+  {
+    return charCount switch
+    {
+      <= 0 => 0,
+      1 => 1,
+      < 10 => 2,
+      < 100 => 3,
+      _ => 4
+    };
+  }
+}
---
+++ LeetCode/1531. String Compression II/Tests.cs
@@ -0,0 +1,20 @@
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace LeetCode._1531._String_Compression_II;
+
+[TestFixture]
+public class Tests
+{
+  [TestCase("aaabcccd", 2, 4)]
+  [TestCase("aabbaa", 2, 2)]
+  [TestCase("aaaaaaaaaaa", 0, 3)]
+  [TestCase("aabaabbcbbbaccc", 6, 4)]
+  [TestCase("abcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyz", 10, 68)]
+  [TestCase("aabbccaabbccaabbccaabbccaabbccaabbccaabbccaabbccaabbccaabbccaabbccaabbcc", 9, 59)]
+  [TestCase("a", 1, 0)]
+  public void Test(string s, int k, int expected)
+  {
+    new Solution().GetLengthOfOptimalCompression(s, k).Should().Be(expected);
+  }
+}
