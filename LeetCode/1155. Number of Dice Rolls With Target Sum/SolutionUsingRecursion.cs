fe1d07d1e84fd28d70aad4f1a7ff48b9f23d9d2c
coffeek <ya.kofeek@yandex.ru>
2023-12-29 00:46:23 +0400

    1155. Number of Dice Rolls With Target Sum

---
+++ LeetCode/1155. Number of Dice Rolls With Target Sum/SolutionUsingRecursion.cs
@@ -0,0 +1,24 @@
+using System.Collections.Generic;
+
+namespace LeetCode._1155._Number_of_Dice_Rolls_With_Target_Sum;
+
+public class SolutionUsingRecursion
+{
+  private readonly Dictionary<(int, int), int> cache = new();
+
+  public int NumRollsToTarget(int n, int k, int target)
+  {
+    if (target <= 0)
+      return 0;
+    if (n == 1)
+      return target <= k ? 1 : 0;
+    var cacheKey = (n, target);
+    if (cache.TryGetValue(cacheKey, out var ans))
+      return ans;
+    const int mod = (int)1e9 + 7;
+    for (var i = 1; i <= k; i++)
+      ans = (ans + NumRollsToTarget(n - 1, k, target - i)) % mod;
+    cache[cacheKey] = ans;
+    return ans;
+  }
+}
---
+++ LeetCode/1155. Number of Dice Rolls With Target Sum/Tests.cs
@@ -0,0 +1,19 @@
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace LeetCode._1155._Number_of_Dice_Rolls_With_Target_Sum;
+
+[TestFixture]
+public class Tests
+{
+  [TestCase(1, 6, 3, 1)]
+  [TestCase(2, 6, 7, 6)]
+  [TestCase(30, 30, 500, 222616187)]
+  [TestCase(2, 12, 8, 7)]
+  public void Test(int n, int k, int target, int expected)
+  {
+    new SolutionUsingRecursion().NumRollsToTarget(n, k, target).Should().Be(expected);
+    new SolutionUsingDp().NumRollsToTarget(n, k, target).Should().Be(expected);
+    new SolutionUsingOptimizedDp().NumRollsToTarget(n, k, target).Should().Be(expected);
+  }
+}
