59739bde5b67d480ff10bf8dfb3508888f1ed9cf
coffeek <ya.kofeek@yandex.ru>
2023-12-30 15:37:44 +0400

    1335. Minimum Difficulty of a Job Schedule

---
+++ LeetCode/1335. Minimum Difficulty of a Job Schedule/DpSolution.cs
@@ -0,0 +1,37 @@
+using System;
+
+namespace LeetCode._1335._Minimum_Difficulty_of_a_Job_Schedule;
+
+public class DpSolution
+{
+  public int MinDifficulty(int[] jobDifficulty, int n)
+  {
+    var m = jobDifficulty.Length;
+    if (n > m)
+      return -1;
+
+    const int inf = (int)1e6;
+    
+    var dp = new int[m];
+    dp[m - 1] = jobDifficulty[m - 1];
+    for (var j = m - 2; j >= 0; j--)
+      dp[j] = Math.Max(dp[j + 1], jobDifficulty[j]);
+
+    var tmp = new int[m];
+    for (var i = 2; i <= n; i++)
+    {
+      Array.Fill(tmp, inf);
+      for (var j = 0; j <= m - i; j++)
+      {
+        var maxDifficulty = 0;
+        for (var k = j; k <= m - i; k++)
+        {
+          maxDifficulty = Math.Max(maxDifficulty, jobDifficulty[k]);
+          tmp[j] = Math.Min(tmp[j], maxDifficulty + dp[k + 1]);
+        }
+      }
+      (tmp, dp) = (dp, tmp);
+    }
+    return dp[0];
+  }
+}
