617d1eeea51342fe4735f480c78cbad8e2ea3bf6
coffeek <ya.kofeek@yandex.ru>
2024-01-02 13:17:18 +0400

    2610. Convert an Array Into a 2D Array

---
+++ LeetCode/2610. Convert an Array Into a 2D Array/Solution.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+
+namespace LeetCode._2610._Convert_an_Array_Into_a_2D_Array;
+
+public class Solution
+{
+  public IList<IList<int>> FindMatrix(int[] nums)
+  {
+    var count = new int[201];
+    foreach (var num in nums)
+      count[num]++;
+    var result = new List<IList<int>>();
+    for (var i = 1; i < count.Length; i++)
+    {
+      for (var j = 0; j < count[i]; j++)
+      {
+        if (j == result.Count)
+          result.Add(new List<int>());
+        result[j].Add(i);
+      }
+    }
+    return result;
+  }
+}
---
+++ LeetCode/2610. Convert an Array Into a 2D Array/Tests.cs
@@ -0,0 +1,30 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+
+namespace LeetCode._2610._Convert_an_Array_Into_a_2D_Array;
+
+[TestFixture]
+public class Tests
+{
+  [Test]
+  public void Test1()
+  {
+    new Solution().FindMatrix(new[] { 1, 3, 4, 1, 2, 3, 1 }).Should().BeEquivalentTo(
+      new[]
+      {
+        new[] { 1, 3, 4, 2 },
+        new[] { 1, 3 },
+        new[] { 1 }
+      });
+  }
+
+  [Test]
+  public void Test2()
+  {
+    new Solution().FindMatrix(new[] { 1, 2, 3, 4 }).Should().BeEquivalentTo(
+      new[]
+      {
+        new[] { 1, 2, 3, 4 }
+      });
+  }
+}
