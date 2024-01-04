95fa8d718a4f507c0c15557f5b4e8bc0e555cd27
coffeek <ya.kofeek@yandex.ru>
2024-01-04 14:00:48 +0400

    1706. Where Will the Ball Fall

---
+++ LeetCode/1706. Where Will the Ball Fall/Solution.cs
@@ -0,0 +1,24 @@
+﻿using System.Linq;
+
+namespace LeetCode._1706._Where_Will_the_Ball_Fall;
+
+public class Solution
+{
+  public int[] FindBall(int[][] grid)
+  {
+    var n = grid[0].Length;
+    return Enumerable.Range(0, n).Select(CalcPos).ToArray();
+
+    int CalcPos(int pos)
+    {
+      foreach (var row in grid)
+      {
+        var newPos = pos + row[pos];
+        if (newPos < 0 || newPos == n || newPos + row[newPos] == pos)
+          return -1;
+        pos = newPos;
+      }
+      return pos;
+    }
+  }
+}
---
+++ LeetCode/1706. Where Will the Ball Fall/Tests.cs
@@ -0,0 +1,35 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+
+namespace LeetCode._1706._Where_Will_the_Ball_Fall;
+
+[TestFixture]
+public class Tests
+{
+  [Test]
+  public void Test1()
+  {
+    new Solution().FindBall(new[]
+    {
+      new[] { 1, 1, 1, -1, -1 },
+      new[] { 1, 1, 1, -1, -1 },
+      new[] { -1, -1, -1, 1, 1 },
+      new[] { 1, 1, 1, 1, -1 },
+      new[] { -1, -1, -1, -1, -1 }
+    }).Should().BeEquivalentTo(new[] { 1, -1, -1, -1, -1 },
+      o => o.WithStrictOrdering());
+  }
+
+  [Test]
+  public void Test2()
+  {
+    new Solution().FindBall(new[]
+    {
+      new[] { 1, 1, 1, 1, 1, 1 },
+      new[] { -1, -1, -1, -1, -1, -1 },
+      new[] { 1, 1, 1, 1, 1, 1 },
+      new[] { -1, -1, -1, -1, -1, -1 }
+    }).Should().BeEquivalentTo(new[] { 0, 1, 2, 3, 4, -1 },
+      o => o.WithStrictOrdering());
+  }
+}
