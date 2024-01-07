79e93a4eaeb080f16946d82b3fb3cc1f7aa90d0e
coffeek <ya.kofeek@yandex.ru>
2024-01-08 01:59:51 +0400

    375. Guess Number Higher or Lower II

---
+++ LeetCode/375. Guess Number Higher or Lower II/Tests.cs
@@ -0,0 +1,18 @@
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace LeetCode._375._Guess_Number_Higher_or_Lower_II;
+
+[TestFixture]
+public class Tests
+{
+  [TestCase(10, 16)]
+  [TestCase(1, 0)]
+  [TestCase(2, 1)]
+  [TestCase(200, 952)]
+  public void Test(int n, int expected)
+  {
+    new DpSolution().GetMoneyAmount(n).Should().Be(expected);
+    new RecursiveSolution().GetMoneyAmount(n).Should().Be(expected);
+  }
+}
