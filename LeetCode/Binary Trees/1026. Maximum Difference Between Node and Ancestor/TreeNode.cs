5b209ace318a3d79429c37a410fdbe659f3a1da0
coffeek <ya.kofeek@yandex.ru>
2024-01-11 12:40:17 +0400

    1026. Maximum Difference Between Node and Ancestor

---
+++ LeetCode/Binary Trees/1026. Maximum Difference Between Node and Ancestor/TreeNode.cs
@@ -0,0 +1,15 @@
+namespace LeetCode.Binary_Trees._1026._Maximum_Difference_Between_Node_and_Ancestor;
+
+public class TreeNode
+{
+  public int val;
+  public TreeNode left;
+  public TreeNode right;
+
+  public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
+  {
+    this.val = val;
+    this.left = left;
+    this.right = right;
+  }
+}
