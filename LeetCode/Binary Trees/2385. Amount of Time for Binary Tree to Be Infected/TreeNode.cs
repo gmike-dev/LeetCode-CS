582dedec453fdf399c803434c02e92ed04a88e1b
coffeek <ya.kofeek@yandex.ru>
2024-01-11 12:40:29 +0400

    2385. Amount of Time for Binary Tree to Be Infected

---
+++ LeetCode/Binary Trees/2385. Amount of Time for Binary Tree to Be Infected/TreeNode.cs
@@ -0,0 +1,15 @@
+namespace LeetCode.Binary_Trees._2385._Amount_of_Time_for_Binary_Tree_to_Be_Infected;
+
+public class TreeNode
+{
+  public int val;
+  public TreeNode left;
+  public TreeNode right;
+
+  public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
+  {
+    this.val = val;
+    this.left = left;
+    this.right = right;
+  }
+}
