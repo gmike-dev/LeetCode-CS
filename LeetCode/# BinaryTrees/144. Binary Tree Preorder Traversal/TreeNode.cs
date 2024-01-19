84092396ab7e6a2c2cf1df3d1a3a026294a64cfb
Городилов Михаил (Gorodilov_MA) <Gorodilov_MA@directum.ru>
2024-01-19 20:21:16 +0400

    144. Binary Tree Preorder Traversal

---
+++ LeetCode/# BinaryTrees/144. Binary Tree Preorder Traversal/TreeNode.cs
@@ -0,0 +1,15 @@
+namespace LeetCode.__BinaryTrees._144._Binary_Tree_Preorder_Traversal;
+
+public class TreeNode
+{
+  public int val;
+  public TreeNode left;
+  public TreeNode right;
+
+  public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
+  {
+    this.val = val;
+    this.left = left;
+    this.right = right;
+  }
+}
