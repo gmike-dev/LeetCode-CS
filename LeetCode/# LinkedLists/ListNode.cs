558724c746987b7ba7c19d147f3230d417554ae0
Городилов Михаил (Gorodilov_MA) <Gorodilov_MA@directum.ru>
2024-01-30 16:57:59 +0400

    19. Remove Nth Node From End of List

---
+++ LeetCode/# LinkedLists/ListNode.cs
@@ -0,0 +1,14 @@
+﻿// ReSharper disable InconsistentNaming
+namespace LeetCode.__LinkedLists;
+
+public class ListNode
+{
+  public int val;
+  public ListNode next;
+
+  public ListNode(int val = 0, ListNode next = null)
+  {
+    this.val = val;
+    this.next = next;
+  }
+}
