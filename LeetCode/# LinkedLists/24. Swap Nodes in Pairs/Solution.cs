b6ffd28b1f3287221282e259a258763714ad3c37
coffeek <ya.kofeek@yandex.ru>
2024-01-30 21:07:37 +0400

    24. Swap Nodes in Pairs

---
+++ LeetCode/# LinkedLists/24. Swap Nodes in Pairs/Solution.cs
@@ -0,0 +1,70 @@
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace LeetCode.__LinkedLists._24._Swap_Nodes_in_Pairs;
+
+public class Solution
+{
+  public ListNode SwapPairs(ListNode head)
+  {
+    if (head?.next == null)
+      return head;
+
+    var dummy = new ListNode
+    {
+      next = head
+    };
+    var prev = dummy;
+    var curr = head;
+    while (curr?.next != null)
+    {
+      prev.next = curr.next;
+      curr.next = curr.next.next;
+      prev.next.next = curr;
+
+      prev = curr;
+      curr = curr.next;
+    }
+    return dummy.next;
+  }
+}
+
+[TestFixture]
+public class Tests
+{
+  [Test]
+  public void Test1()
+  {
+    new Solution()
+      .SwapPairs(new ListNode(1, new(2, new(3, new(4)))))
+      .Should()
+      .BeEquivalentTo(new ListNode(2, new(1, new(4, new(3)))));
+  }
+
+  [Test]
+  public void Test2()
+  {
+    new Solution()
+      .SwapPairs(new ListNode(1))
+      .Should()
+      .BeEquivalentTo(new ListNode(1));
+  }
+
+  [Test]
+  public void Test3()
+  {
+    new Solution()
+      .SwapPairs(null)
+      .Should()
+      .BeNull();
+  }
+
+  [Test]
+  public void Test4()
+  {
+    new Solution()
+      .SwapPairs(new ListNode(1, new(2, new(3))))
+      .Should()
+      .BeEquivalentTo(new ListNode(2, new(1, new(3))));
+  }
+}
