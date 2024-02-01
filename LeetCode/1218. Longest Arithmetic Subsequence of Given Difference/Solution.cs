61101edccd20e6a7591d40ea2c84b80ed8c57cb9
coffeek <ya.kofeek@yandex.ru>
2024-02-01 10:50:52 +0400

    1218. Longest Arithmetic Subsequence of Given Difference (short solution)

--- LeetCode/1218. Longest Arithmetic Subsequence of Given Difference/Solution.cs
+++ LeetCode/1218. Longest Arithmetic Subsequence of Given Difference/Solution.cs
@@ -1,5 +1,7 @@
 using System.Collections.Generic;
 using System.Linq;
+using FluentAssertions;
+using NUnit.Framework;
 
 namespace LeetCode._1218._Longest_Arithmetic_Subsequence_of_Given_Difference;
 
@@ -11,11 +13,21 @@ public class Solution
     foreach (var x in arr)
     {
       var prev = x - difference;
-      if (d.TryGetValue(prev, out var prevLength))
-        d[x] = prevLength + 1;
-      else
-        d[x] = 1;
+      d[x] = d.GetValueOrDefault(prev, 0) + 1;
     }
     return d.Values.Max();
+  }
+}
+
+[TestFixture]
+public class Tests
+{
+  [TestCase(new[] { 1, 2, 3, 4 }, 1, 4)]
+  [TestCase(new[] { 1, 3, 5, 7 }, 1, 1)]
+  [TestCase(new[] { 1, 3, 5, 7 }, 2, 4)]
+  [TestCase(new[] { 1, 5, 7, 8, 5, 3, 4, 2, 1 }, -2, 4)]
+  public void Test1(int[] arr, int difference, int expected)
+  {
+    new Solution().LongestSubsequence(arr, difference).Should().Be(expected);
   }
 }
