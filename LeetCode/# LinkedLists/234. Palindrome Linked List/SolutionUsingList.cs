dad23b7a69483ef5438e7dbad0f1ce7c27e3cbef
coffeek <ya.kofeek@yandex.ru>
2024-03-22 11:06:20 +0400

    234. Palindrome Linked List (new solutions added)

---
+++ LeetCode/# LinkedLists/234. Palindrome Linked List/SolutionUsingList.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections.Generic;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace LeetCode.__LinkedLists._234._Palindrome_Linked_List;
+
+public class SolutionUsingList
+{
+  public bool IsPalindrome(ListNode head)
+  {
+    var list = new List<int>();
+    for (var node = head; node != null; node = node.next)
+      list.Add(node.val);
+    for (var i = 0; i + i < list.Count; i++)
+    {
+      if (list[i] != list[^(i + 1)])
+        return false;
+    }
+    return true;
+  }
+}
+
+[TestFixture]
+public class SolutionUsingListTests
+{
+  [Test]
+  public void Test1()
+  {
+    var list = new ListNode(1, new ListNode(2, new ListNode(2, new ListNode(1))));
+    new SolutionUsingList().IsPalindrome(list).Should().BeTrue();
+  }
+
+  [Test]
+  public void Test2()
+  {
+    var list = new ListNode(1, new ListNode(2));
+    new SolutionUsingList().IsPalindrome(list).Should().BeFalse();
+  }
+
+  [Test]
+  public void Test3()
+  {
+    var list = new ListNode(1, new ListNode(0, new ListNode(0)));
+    new SolutionUsingList().IsPalindrome(list).Should().BeFalse();
+  }
+
+  [Test]
+  public void Test4()
+  {
+    var list = new ListNode(1);
+    new SolutionUsingList().IsPalindrome(list).Should().BeTrue();
+  }
+}
---
+++ LeetCode/# LinkedLists/234. Palindrome Linked List/SolutionUsingStackSlowFast.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections.Generic;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace LeetCode.__LinkedLists._234._Palindrome_Linked_List;
+
+public class SolutionUsingStackSlowFast
+{
+  public bool IsPalindrome(ListNode head)
+  {
+    var stack = new Stack<int>();
+    var slow = head;
+    var fast = head;
+    while (fast?.next != null)
+    {
+      fast = fast.next?.next;
+      stack.Push(slow.val);
+      slow = slow.next;
+    }
+    if (fast != null)
+      slow = slow.next;
+    while (slow != null)
+    {
+      if (stack.Pop() != slow.val)
+        return false;
+      slow = slow.next;
+    }
+    return true;
+  }
+}
+
+[TestFixture]
+public class SolutionUsingStackSlowFastTests
+{
+  [Test]
+  public void Test1()
+  {
+    var list = new ListNode(1, new ListNode(2, new ListNode(2, new ListNode(1))));
+    new SolutionUsingStackSlowFast().IsPalindrome(list).Should().BeTrue();
+  }
+
+  [Test]
+  public void Test2()
+  {
+    var list = new ListNode(1, new ListNode(2));
+    new SolutionUsingStackSlowFast().IsPalindrome(list).Should().BeFalse();
+  }
+
+  [Test]
+  public void Test3()
+  {
+    var list = new ListNode(1, new ListNode(0, new ListNode(0)));
+    new SolutionUsingStackSlowFast().IsPalindrome(list).Should().BeFalse();
+  }
+
+  [Test]
+  public void Test4()
+  {
+    var list = new ListNode(1);
+    new SolutionUsingStackSlowFast().IsPalindrome(list).Should().BeTrue();
+  }
+}
