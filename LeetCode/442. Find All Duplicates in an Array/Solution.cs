1eedd755b2419b059eabd84867a8fdba884a180c
coffeek <ya.kofeek@yandex.ru>
2024-03-25 10:29:19 +0400

    442. Find All Duplicates in an Array

---
+++ LeetCode/442. Find All Duplicates in an Array/Solution.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace LeetCode._442._Find_All_Duplicates_in_an_Array;
+
+public class Solution
+{
+  public IList<int> FindDuplicates(int[] a)
+  {
+    var result = new List<int>();
+    var n = a.Length;
+    for (var i = 0; i < n; i++)
+    {
+      var j = Math.Abs(a[i]) - 1;
+      if (a[j] < 0)
+        result.Add(j + 1);
+      a[j] = -a[j];
+    }
+    return result;
+  }
+}
+
+[TestFixture]
+public class Tests
+{
+  [TestCase(new[] { 4, 3, 2, 7, 8, 2, 3, 1 }, new[] { 2, 3 })]
+  [TestCase(new[] { 1, 1, 2 }, new[] { 1 })]
+  [TestCase(new[] { 1 }, new int[] { })]
+  [TestCase(new[] { 2, 2, 3, 1, 3 }, new[] { 2, 3 })]
+  [TestCase(new[] { 2, 2, 3, 1, 3, 1 }, new[] { 1, 2, 3 })]
+  public void Test(int[] a, int[] expected)
+  {
+    new Solution().FindDuplicates(a).Should().BeEquivalentTo(expected);
+  }
+}
