400fb8335ee9d0c6047cb07a959415f3343f6361
coffeek <ya.kofeek@yandex.ru>
2024-04-05 07:57:57 +0400

    1544. Make The String Great

---
+++ LeetCode/# Strings/1544. Make The String Great/Solution2.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LeetCode.__Strings._1544._Make_The_String_Great;
+
+public class Solution2
+{
+  public string MakeGood(string s)
+  {
+    var stack = new Stack<char>(s.Length);
+    foreach (var c in s)
+    {
+      if (stack.TryPeek(out var prev) && Math.Abs(c - prev) == 'a' - 'A')
+        stack.Pop();
+      else
+        stack.Push(c);
+    }
+    return new string(stack.Reverse().ToArray());
+  }
+}
---
+++ LeetCode/# Strings/1544. Make The String Great/Tests.cs
@@ -0,0 +1,17 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+
+namespace LeetCode.__Strings._1544._Make_The_String_Great;
+
+[TestFixture]
+public class Tests
+{
+  [TestCase("leEeetcode", "leetcode")]
+  [TestCase("abBAcC", "")]
+  [TestCase("s", "s")]
+  public void Test(string s, string expected)
+  {
+    new Solution1().MakeGood(s).Should().Be(expected);
+    new Solution2().MakeGood(s).Should().Be(expected);
+  }
+}
