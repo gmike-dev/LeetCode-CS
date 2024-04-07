aa4cec8c584905b3bbb399db3e175b322bec350c
coffeek <ya.kofeek@yandex.ru>
2024-04-07 13:27:16 +0400

    1249. Minimum Remove to Make Valid Parentheses

---
+++ LeetCode/# Strings/1249. Minimum Remove to Make Valid Parentheses/Solution.cs
@@ -0,0 +1,42 @@
+﻿namespace LeetCode.__Strings._1249._Minimum_Remove_to_Make_Valid_Parentheses;
+
+public class Solution
+{
+  public string MinRemoveToMakeValid(string s)
+  {
+    var close = s.Count(c => c == ')');
+    var open = 0;
+    var result = new StringBuilder();
+    foreach (var c in s)
+    {
+      if (c == '(')
+      {
+        if (open == close)
+          continue;
+        open++;
+      }
+      else if (c == ')')
+      {
+        close--;
+        if (open == 0)
+          continue;
+        open--;
+      }
+      result.Append(c);
+    }
+    return result.ToString();
+  }
+}
+
+[TestFixture]
+public class Tests
+{
+  [TestCase("lee(t(c)o)de)", "lee(t(c)o)de")]
+  [TestCase("a)b(c)d", "ab(c)d")]
+  [TestCase("))((", "")]
+  [TestCase("(a(b(c)d)", "(a(bc)d)")]
+  public void Test(string s, string expected)
+  {
+    new Solution().MinRemoveToMakeValid(s).Should().Be(expected);
+  }
+}
