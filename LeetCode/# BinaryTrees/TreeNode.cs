c0290bc77269ada64a057dd0fef2b43cc37556a9
Mike Gorodilov <mihailgorodilov@gmail.com>
2024-06-26 20:25:23 +0400

    1382. Balance a Binary Search Tree

--- LeetCode/# BinaryTrees/TreeNode.cs
+++ LeetCode/# BinaryTrees/TreeNode.cs
@@ -1,17 +1,120 @@
 // ReSharper disable FieldCanBeMadeReadOnly.Global
 // ReSharper disable InconsistentNaming
+
 namespace LeetCode.__BinaryTrees;
 
-public class TreeNode
+public class TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
 {
-  public int val;
-  public TreeNode left;
-  public TreeNode right;
+  public int val = val;
+  public TreeNode left = left;
+  public TreeNode right = right;
+}
 
-  public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
+public static class TreeNodeExtensions
+{
+  public static TreeNode FromString(string s)
   {
-    this.val = val;
-    this.left = left;
-    this.right = right;
+    if (string.IsNullOrWhiteSpace(s))
+      return null;
+    var values = s.Split(',');
+    var root = new TreeNode(int.Parse(values[0]));
+    var pos = 1;
+    var q = new Queue<TreeNode>();
+    q.Enqueue(root);
+    while (pos < values.Length)
+    {
+      var node = q.Dequeue();
+      if (pos < values.Length && values[pos] != "null")
+      {
+        var value = int.Parse(values[pos]);
+        node.left = new TreeNode(value);
+        q.Enqueue(node.left);
+      }
+      pos++;
+      if (pos < values.Length && values[pos] != "null")
+      {
+        var value = int.Parse(values[pos]);
+        node.right = new TreeNode(value);
+        q.Enqueue(node.right);
+      }
+      pos++;
+    }
+    return root;
+  }
+
+  public static string ToString(TreeNode root)
+  {
+    if (root == null)
+      return "";
+    List<string> values = [];
+    var q = new Queue<TreeNode>();
+    q.Enqueue(root);
+    while (q.Count > 0)
+    {
+      var node = q.Dequeue();
+      if (node == null)
+      {
+        values.Add("null");
+      }
+      else
+      {
+        values.Add(node.val.ToString());
+        q.Enqueue(node.left);
+        q.Enqueue(node.right);
+      }
+    }
+    while (values.Count > 0 && values[^1] == "null")
+      values.RemoveAt(values.Count - 1);
+    return string.Join(',', values);
+  }
+}
+
+[TestFixture]
+public class TreeNodeExtensionsTests
+{
+  [Test]
+  public void FromStringTest()
+  {
+    TreeNodeExtensions.FromString("1,10,4,3,null,7,9,12,8,6,null,null,2").Should().BeEquivalentTo(
+      new TreeNode(1,
+        new TreeNode(10,
+          new TreeNode(3,
+            new TreeNode(12),
+            new TreeNode(8))),
+        new TreeNode(4,
+          new TreeNode(7,
+            new TreeNode(6)),
+          new TreeNode(9,
+            null,
+            new TreeNode(2)))));
+  }
+
+  [Test]
+  public void FromStringTest_EmptyTree()
+  {
+    TreeNodeExtensions.FromString("").Should().BeNull();
+  }
+
+  [Test]
+  public void ToStringTest()
+  {
+    var root = new TreeNode(1,
+      new TreeNode(10,
+        new TreeNode(3,
+          new TreeNode(12),
+          new TreeNode(8))),
+      new TreeNode(4,
+        new TreeNode(7,
+          new TreeNode(6)),
+        new TreeNode(9,
+          null,
+          new TreeNode(2))));
+    TreeNodeExtensions.ToString(root).Should().Be("1,10,4,3,null,7,9,12,8,6,null,null,2");
+  }
+
+  [Test]
+  public void ToStringTest_EmptyTree()
+  {
+    TreeNodeExtensions.ToString(null).Should().Be("");
   }
 }
