b877a0f535cdcb5903c3b037321230ae8c4f8e5a
Mike Gorodilov <mihailgorodilov@gmail.com>
2024-09-23 03:08:07 +0400

    235. Lowest Common Ancestor of a Binary Search Tree

---
+++ LeetCode/# BinaryTrees/235. Lowest Common Ancestor of a Binary Search Tree/Solution.cs
@@ -0,0 +1,54 @@
+namespace LeetCode.__BinaryTrees._235._Lowest_Common_Ancestor_of_a_Binary_Search_Tree;
+
+public class Solution
+{
+  public TreeNode LowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q)
+  {
+    if (p.val > q.val)
+      (p, q) = (q, p);
+    while (true)
+    {
+      if (p.val == root.val)
+        return p;
+      if (q.val == root.val)
+        return q;
+      if (q.val < root.val)
+        root = root.left;
+      else if (p.val > root.val)
+        root = root.right;
+      else
+        return root;
+    }
+  }
+}
+
+[TestFixture]
+public class SolutionTests
+{
+  [Test]
+  public void Test1()
+  {
+    new Solution()
+      .LowestCommonAncestor(TreeNode.FromString("[6,2,8,0,4,7,9,null,null,3,5]"), new TreeNode(2), new TreeNode(8))
+      .val
+      .Should().Be(6);
+  }
+
+  [Test]
+  public void Test2()
+  {
+    new Solution()
+      .LowestCommonAncestor(TreeNode.FromString("[6,2,8,0,4,7,9,null,null,3,5]"), new TreeNode(2), new TreeNode(4))
+      .val
+      .Should().Be(2);
+  }
+
+  [Test]
+  public void Test3()
+  {
+    new Solution()
+      .LowestCommonAncestor(TreeNode.FromString("[2,1]"), new TreeNode(2), new TreeNode(1))
+      .val
+      .Should().Be(2);
+  }
+}
