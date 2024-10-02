cee09e7faf6d4b204514c97b69f300f80d01ae12
Mike Gorodilov <mihailgorodilov@gmail.com>
2024-10-02 13:35:54 +0400

    1331. Rank Transform of an Array

---
+++ LeetCode/1331. Rank Transform of an Array/Solution1.cs
@@ -0,0 +1,37 @@
+﻿namespace LeetCode._1331._Rank_Transform_of_an_Array;
+
+public class Solution1
+{
+  public int[] ArrayRankTransform(int[] arr)
+  {
+    var d = new SortedDictionary<int, List<int>>();
+    for (var i = 0; i < arr.Length; i++)
+    {
+      if (d.TryGetValue(arr[i], out var list))
+        list.Add(i);
+      else
+        d[arr[i]] = [i];
+    }
+    var ranks = new int[arr.Length];
+    var rank = 1;
+    foreach (var x in d.Keys)
+    {
+      foreach (var i in d[x])
+        ranks[i] = rank;
+      rank++;
+    }
+    return ranks;
+  }
+}
+
+[TestFixture]
+public class Solution1Tests
+{
+  [TestCase(new[] { 40, 10, 20, 30 }, new[] { 4, 1, 2, 3 })]
+  [TestCase(new[] { 100, 100, 100 }, new[] { 1, 1, 1 })]
+  [TestCase(new[] { 37, 12, 28, 9, 100, 56, 80, 5, 12 }, new[] { 5, 3, 4, 2, 8, 6, 7, 1, 3 })]
+  public void Test(int[] arr, int[] expected)
+  {
+    new Solution1().ArrayRankTransform(arr).Should().BeEquivalentTo(expected, o => o.WithStrictOrdering());
+  }
+}
---
+++ LeetCode/1331. Rank Transform of an Array/Solution2.cs
@@ -0,0 +1,35 @@
+﻿namespace LeetCode._1331._Rank_Transform_of_an_Array;
+
+public class Solution2
+{
+  public int[] ArrayRankTransform(int[] arr)
+  {
+    var n = arr.Length;
+    var indexes = new int[n];
+    for (var i = 0; i < n; i++)
+      indexes[i] = i;
+    indexes.AsSpan().Sort((x, y) => arr[x] - arr[y]);
+    var rank = 0;
+    var ranks = new int[n];
+    for (var i = 0; i < n; i++)
+    {
+      if (i == 0 || arr[indexes[i]] != arr[indexes[i - 1]])
+        rank++;
+      ranks[indexes[i]] = rank;
+    }
+    return ranks;
+  }
+}
+
+[TestFixture]
+public class Solution2Tests
+{
+  [TestCase(new[] { 40, 10, 20, 30 }, new[] { 4, 1, 2, 3 })]
+  [TestCase(new[] { 100, 100, 100 }, new[] { 1, 1, 1 })]
+  [TestCase(new[] { 37, 12, 28, 9, 100, 56, 80, 5, 12 }, new[] { 5, 3, 4, 2, 8, 6, 7, 1, 3 })]
+  [TestCase(new int[0], new int[0])]
+  public void Test(int[] arr, int[] expected)
+  {
+    new Solution2().ArrayRankTransform(arr).Should().BeEquivalentTo(expected, o => o.WithStrictOrdering());
+  }
+}
