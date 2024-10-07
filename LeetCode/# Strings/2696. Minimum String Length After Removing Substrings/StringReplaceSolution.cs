ffa503fc2a97d3a8ad77ccda79169cdd8ec846d2
Mike Gorodilov <mihailgorodilov@gmail.com>
2024-10-07 11:08:43 +0400

    2696. Minimum String Length After Removing Substrings

---
+++ LeetCode/# Strings/2696. Minimum String Length After Removing Substrings/StringReplaceSolution.cs
@@ -0,0 +1,26 @@
+﻿namespace LeetCode.__Strings._2696._Minimum_String_Length_After_Removing_Substrings;
+
+public class StringReplaceSolution
+{
+  public int MinLength(string s)
+  {
+    while (true)
+    {
+      var ss = s.Replace("AB", "").Replace("CD", "");
+      if (ss.Length == s.Length)
+        return ss.Length;
+      s = ss;
+    }
+  }
+}
+
+[TestFixture]
+public class StringReplaceSolutionTests
+{
+  [TestCase("ABFCACDB", 2)]
+  [TestCase("ACBBD", 5)]
+  public void Test(string s, int expected)
+  {
+    new StringReplaceSolution().MinLength(s).Should().Be(expected);
+  }
+}
