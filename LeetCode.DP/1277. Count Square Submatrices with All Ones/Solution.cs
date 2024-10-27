f9a042321e7d5d03ef5875de869427c614595de6
Mike Gorodilov <mihailgorodilov@gmail.com>
2024-10-27 11:23:49 +0400

    1277. Count Square Submatrices with All Ones

---
+++ LeetCode.DP/1277. Count Square Submatrices with All Ones/Solution.cs
@@ -0,0 +1,50 @@
+﻿namespace LeetCode.DP._1277._Count_Square_Submatrices_with_All_Ones;
+
+public class Solution
+{
+  public int CountSquares(int[][] matrix)
+  {
+    var n = matrix.Length;
+    var m = matrix[0].Length;
+    var dp = new int[n][];
+    for (var i = 0; i < n; i++)
+      dp[i] = new int[m];
+    for (var i = 0; i < n; i++)
+      dp[i][0] = matrix[i][0];
+    for (var j = 0; j < m; j++)
+      dp[0][j] = matrix[0][j];
+    for (var i = 1; i < n; i++)
+    {
+      for (var j = 1; j < m; j++)
+      {
+        if (matrix[i][j] != 0)
+          dp[i][j] = Math.Min(dp[i - 1][j - 1], Math.Min(dp[i][j - 1], dp[i - 1][j])) + 1;
+      }
+    }
+    return dp.SelectMany(r => r).Sum();
+  }
+}
+
+[TestFixture]
+public class SolutionTests
+{
+  [Test]
+  public void Test1()
+  {
+    new Solution().CountSquares([
+      [0, 1, 1, 1],
+      [1, 1, 1, 1],
+      [0, 1, 1, 1]
+    ]).Should().Be(15);
+  }
+
+  [Test]
+  public void Test2()
+  {
+    new Solution().CountSquares([
+      [1, 0, 1],
+      [1, 1, 0],
+      [1, 1, 0]
+    ]).Should().Be(7);
+  }
+}
---
+++ LeetCode.DP/1277. Count Square Submatrices with All Ones/Solution2.cs
@@ -0,0 +1,61 @@
+﻿namespace LeetCode.DP._1277._Count_Square_Submatrices_with_All_Ones;
+
+public class Solution2
+{
+  public int CountSquares(int[][] matrix)
+  {
+    var m = matrix.Length;
+    var n = matrix[0].Length;
+    var prev = new int[n];
+    var curr = new int[n];
+    var count = 0;
+    for (var j = 0; j < n; j++)
+    {
+      prev[j] = matrix[0][j];
+      count += matrix[0][j];
+    }
+    for (var i = 1; i < m; i++)
+    {
+      curr[0] = matrix[i][0];
+      count += matrix[i][0];
+      for (var j = 1; j < n; j++)
+      {
+        if (matrix[i][j] == 0)
+        {
+          curr[j] = 0;
+        }
+        else
+        {
+          curr[j] = Math.Min(curr[j - 1], Math.Min(prev[j - 1], prev[j])) + 1;
+          count += curr[j];
+        }
+      }
+      (prev, curr) = (curr, prev);
+    }
+    return count;
+  }
+}
+
+[TestFixture]
+public class Solution2Tests
+{
+  [Test]
+  public void Test1()
+  {
+    new Solution2().CountSquares([
+      [0, 1, 1, 1],
+      [1, 1, 1, 1],
+      [0, 1, 1, 1]
+    ]).Should().Be(15);
+  }
+
+  [Test]
+  public void Test2()
+  {
+    new Solution2().CountSquares([
+      [1, 0, 1],
+      [1, 1, 0],
+      [1, 1, 0]
+    ]).Should().Be(7);
+  }
+}
