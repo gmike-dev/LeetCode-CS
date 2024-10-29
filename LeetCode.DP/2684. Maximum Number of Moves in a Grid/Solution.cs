826b3c5c5f7a4c58408154a5d9a2fc418d079d27
Mike Gorodilov <mihailgorodilov@gmail.com>
2024-10-29 11:10:02 +0400

    2684. Maximum Number of Moves in a Grid

---
+++ LeetCode.DP/2684. Maximum Number of Moves in a Grid/Solution.cs
@@ -0,0 +1,61 @@
+namespace LeetCode.DP._2684._Maximum_Number_of_Moves_in_a_Grid;
+
+public class Solution
+{
+  public int MaxMoves(int[][] grid)
+  {
+    var m = grid.Length;
+    var n = grid[0].Length;
+    var dp = new int[m][];
+    for (var i = 0; i < m; i++)
+      dp[i] = new int[n];
+    for (var j = n - 1; j > 0; j--)
+    {
+      for (var i = 0; i < m; i++)
+      {
+        if (i > 0 && grid[i][j] > grid[i - 1][j - 1])
+          dp[i - 1][j - 1] = Math.Max(dp[i - 1][j - 1], dp[i][j] + 1);
+        if (grid[i][j] > grid[i][j - 1])
+          dp[i][j - 1] = Math.Max(dp[i][j - 1], dp[i][j] + 1);
+        if (i < m - 1 && grid[i][j] > grid[i + 1][j - 1])
+          dp[i + 1][j - 1] = Math.Max(dp[i + 1][j - 1], dp[i][j] + 1);
+      }
+    }
+    return Enumerable.Range(0, m).Max(i => dp[i][0]);
+  }
+}
+
+[TestFixture]
+public class SolutionTests
+{
+  [Test]
+  public void Test1()
+  {
+    new Solution().MaxMoves([[2, 4, 3, 5], [5, 4, 9, 3], [3, 4, 2, 11], [10, 9, 13, 15]]).Should().Be(3);
+  }
+
+  [Test]
+  public void Test2()
+  {
+    new Solution().MaxMoves([[3, 2, 4], [2, 1, 9], [1, 1, 7]]).Should().Be(0);
+  }
+
+  [Test]
+  public void Test3()
+  {
+    new Solution().MaxMoves([
+      [65, 200, 263, 220, 91, 183, 2, 187, 175, 61, 225, 120, 39],
+      [111, 242, 294, 31, 241, 90, 145, 25, 262, 214, 145, 71, 294],
+      [152, 25, 240, 69, 279, 238, 222, 9, 137, 277, 8, 143, 143],
+      [189, 31, 86, 250, 20, 63, 188, 209, 75, 22, 127, 272, 110],
+      [122, 94, 298, 25, 90, 169, 68, 3, 208, 274, 202, 135, 275],
+      [205, 20, 171, 90, 70, 272, 280, 138, 142, 151, 80, 122, 130],
+      [284, 272, 271, 269, 265, 134, 185, 243, 247, 50, 283, 20, 232],
+      [266, 236, 265, 234, 249, 62, 98, 130, 122, 226, 285, 168, 204],
+      [231, 24, 256, 101, 142, 28, 268, 82, 111, 63, 115, 13, 144],
+      [277, 277, 31, 144, 49, 132, 28, 138, 133, 29, 286, 45, 93],
+      [163, 96, 25, 9, 3, 159, 148, 59, 25, 81, 233, 127, 12],
+      [127, 38, 31, 209, 300, 256, 15, 43, 74, 64, 73, 141, 200]
+    ]).Should().Be(3);
+  }
+}
