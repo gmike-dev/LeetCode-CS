1fa35601a95ba9bc425711917791571bbbef588d
Mike Gorodilov <mihailgorodilov@gmail.com>
2024-11-21 11:22:41 +0400

    2257. Count Unguarded Cells in the Grid

---
+++ LeetCode/2257. Count Unguarded Cells in the Grid/Solution.cs
@@ -0,0 +1,85 @@
+namespace LeetCode._2257._Count_Unguarded_Cells_in_the_Grid;
+
+public class Solution
+{
+  public int CountUnguarded(int m, int n, int[][] guards, int[][] walls)
+  {
+    var grid = new int[m, n];
+    foreach (var g in guards)
+      grid[g[0], g[1]] = 2;
+    foreach (var w in walls)
+      grid[w[0], w[1]] = 3;
+    for (var i = 0; i < m; i++)
+    {
+      var prev = 0;
+      for (var j = 0; j < n; j++)
+      {
+        if (grid[i, j] > 1)
+          prev = grid[i, j];
+        else if (prev == 2)
+          grid[i, j] = 1;
+      }
+    }
+    for (var j = 0; j < n; j++)
+    {
+      var prev = 0;
+      for (var i = 0; i < m; i++)
+      {
+        if (grid[i, j] > 1)
+          prev = grid[i, j];
+        else if (prev == 2)
+          grid[i, j] = 1;
+      }
+    }
+    for (var j = 0; j < n; j++)
+    {
+      var prev = 0;
+      for (var i = m - 1; i >= 0; i--)
+      {
+        if (grid[i, j] > 1)
+          prev = grid[i, j];
+        else if (prev == 2)
+          grid[i, j] = 1;
+      }
+    }
+    for (var i = 0; i < m; i++)
+    {
+      var prev = 0;
+      for (var j = n - 1; j >= 0; j--)
+      {
+        if (grid[i, j] > 1)
+          prev = grid[i, j];
+        else if (prev == 2)
+          grid[i, j] = 1;
+      }
+    }
+    var count = 0;
+    for (var i = 0; i < m; i++)
+    {
+      for (var j = 0; j < n; j++)
+      {
+        if (grid[i, j] == 0)
+          count++;
+      }
+    }
+    return count;
+  }
+}
+
+[TestFixture]
+public class SolutionTests
+{
+  [Test]
+  public void Test1()
+  {
+    new Solution().CountUnguarded(4, 6, [[0, 0], [1, 1], [2, 3]], [[0, 1], [2, 2], [1, 4]])
+      .Should().Be(7);
+  }
+
+  [Test]
+  public void Test2()
+  {
+    new Solution().CountUnguarded(3, 3, [[1, 1]], [[0, 1], [1, 0], [2, 1], [1, 2]])
+      .Should().Be(4);
+  }
+}
