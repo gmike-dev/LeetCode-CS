49e73c9cd6ee72da4501caa1c7c11d79bef16726
Mike Gorodilov <mihailgorodilov@gmail.com>
2024-11-23 12:21:44 +0400

    1861. Rotating the Box (solution #2)

--- LeetCode/1861. Rotating the Box/Solution2.cs
+++ LeetCode/1861. Rotating the Box/Solution2.cs
@@ -1,54 +1,45 @@
 namespace LeetCode._1861._Rotating_the_Box;
 
-public class Solution
+public class Solution2
 {
   public char[][] RotateTheBox(char[][] box)
   {
     var m = box.Length;
     var n = box[0].Length;
-    var shiftedBox = new char[m][];
-    for (var i = 0; i < m; i++)
-    {
-      shiftedBox[i] = new char[n];
-      shiftedBox[i].AsSpan().Fill('.');
-    }
-    for (var i = 0; i < m; i++)
-    {
-      var stones = 0;
-      for (var j = 0; j < n; j++)
-      {
-        if (box[i][j] == '#')
-        {
-          stones++;
-        }
-        else if (box[i][j] == '*')
-        {
-          shiftedBox[i][j] = '*';
-          shiftedBox[i].AsSpan(j - stones, stones).Fill('#');
-          stones = 0;
-        }
-      }
-      shiftedBox[i].AsSpan(n - stones, stones).Fill('#');
-    }
     var result = new char[n][];
     for (var i = 0; i < n; i++)
-      result[i] = new char[m];
-    for (var j = 0; j < n; j++)
     {
-      for (var i = 0; i < m; i++)
-        result[j][m - i - 1] = shiftedBox[i][j];
+      result[i] = new char[m];
+      result[i].AsSpan().Fill('.');
+    }
+    for (var i = 0; i < m; i++)
+    {
+      var lowestEmptyRow = n - 1;
+      for (var j = n - 1; j >= 0; j--)
+      {
+        switch (box[i][j])
+        {
+          case '#':
+            result[lowestEmptyRow--][m - i - 1] = '#';
+            break;
+          case '*':
+            result[j][m - i - 1] = '*';
+            lowestEmptyRow = j - 1;
+            break;
+        }
+      }
     }
     return result;
   }
 }
 
 [TestFixture]
-public class SolutionTests
+public class Solution2Tests
 {
   [Test]
   public void Test1()
   {
-    new Solution().RotateTheBox([['#', '.', '#']]).Should().BeEquivalentTo(new char[][]
+    new Solution2().RotateTheBox([['#', '.', '#']]).Should().BeEquivalentTo(new char[][]
     {
       ['.'],
       ['#'],
@@ -59,7 +50,7 @@ public class SolutionTests
   [Test]
   public void Test2()
   {
-    new Solution().RotateTheBox([
+    new Solution2().RotateTheBox([
       ['#', '.', '*', '.'],
       ['#', '#', '*', '.']
     ]).Should().BeEquivalentTo(new char[][]
@@ -74,7 +65,7 @@ public class SolutionTests
   [Test]
   public void Test3()
   {
-    new Solution().RotateTheBox([
+    new Solution2().RotateTheBox([
       ['#', '#', '*', '.', '*', '.'],
       ['#', '#', '#', '*', '.', '.'],
       ['#', '#', '#', '.', '#', '.']
