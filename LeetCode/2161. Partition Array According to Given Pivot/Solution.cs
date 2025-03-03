f4d5ccdf670071c8d30561902726bffc3f277c88
Mike Gorodilov <mihailgorodilov@gmail.com>
2025-03-03 12:53:52 +0400

    2161. Partition Array According to Given Pivot

---
+++ LeetCode/2161. Partition Array According to Given Pivot/Solution.cs
@@ -0,0 +1,36 @@
+﻿namespace LeetCode._2161._Partition_Array_According_to_Given_Pivot;
+
+public class Solution
+{
+  public int[] PivotArray(int[] nums, int pivot)
+  {
+    var lessThanPivot = new List<int>();
+    var greaterThanPivot = new List<int>();
+    foreach (var num in nums)
+    {
+      if (num < pivot)
+        lessThanPivot.Add(num);
+      else if (num > pivot)
+        greaterThanPivot.Add(num);
+    }
+    var index = 0;
+    foreach (var num in lessThanPivot)
+      nums[index++] = num;
+    for (var i = 0; i < nums.Length - lessThanPivot.Count - greaterThanPivot.Count; i++)
+      nums[index++] = pivot;
+    foreach (var num in greaterThanPivot)
+      nums[index++] = num;
+    return nums;
+  }
+}
+
+[TestFixture]
+public class SolutionTests
+{
+  [TestCase(new[] { 9, 12, 5, 10, 14, 3, 10 }, 10, new[] { 9, 5, 3, 10, 10, 12, 14 })]
+  [TestCase(new[] { -3, 4, 3, 2 }, 2, new[] { -3, 2, 4, 3 })]
+  public void Test(int[] nums, int pivot, int[] expected)
+  {
+    new Solution().PivotArray(nums, pivot).Should().BeEquivalentTo(expected, o => o.WithStrictOrdering());
+  }
+}
---
+++ LeetCode/2161. Partition Array According to Given Pivot/Solution2.cs
@@ -0,0 +1,36 @@
+﻿namespace LeetCode._2161._Partition_Array_According_to_Given_Pivot;
+
+public class Solution2
+{
+  public int[] PivotArray(int[] nums, int pivot)
+  {
+    var n = nums.Length;
+    Span<int> less = stackalloc int[n];
+    Span<int> greater = stackalloc int[n];
+    var lessCount = 0;
+    var greaterCount = 0;
+    foreach (var num in nums)
+    {
+      if (num < pivot)
+        less[lessCount++] = num;
+      else if (num > pivot)
+        greater[greaterCount++] = num;
+    }
+    less[..lessCount].CopyTo(nums.AsSpan(0, lessCount));
+    var pivotCount = nums.Length - lessCount - greaterCount;
+    nums.AsSpan(lessCount, pivotCount).Fill(pivot);
+    greater[..greaterCount].CopyTo(nums.AsSpan()[(lessCount + pivotCount)..]);
+    return nums;
+  }
+}
+
+[TestFixture]
+public class Solution2Tests
+{
+  [TestCase(new[] { 9, 12, 5, 10, 14, 3, 10 }, 10, new[] { 9, 5, 3, 10, 10, 12, 14 })]
+  [TestCase(new[] { -3, 4, 3, 2 }, 2, new[] { -3, 2, 4, 3 })]
+  public void Test(int[] nums, int pivot, int[] expected)
+  {
+    new Solution2().PivotArray(nums, pivot).Should().BeEquivalentTo(expected, o => o.WithStrictOrdering());
+  }
+}
