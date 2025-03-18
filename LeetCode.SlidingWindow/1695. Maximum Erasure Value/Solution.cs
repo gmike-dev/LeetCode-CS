36552eeb1fe0ae65b6b01c202d6f0bc707b62717
Mike Gorodilov <mihailgorodilov@gmail.com>
2025-03-18 14:52:36 +0400

    1695. Maximum Erasure Value

---
+++ LeetCode.SlidingWindow/1695. Maximum Erasure Value/Solution.cs
@@ -0,0 +1,39 @@
+﻿namespace LeetCode.SlidingWindow._1695._Maximum_Erasure_Value;
+
+public class Solution
+{
+  public int MaximumUniqueSubarray(int[] nums)
+  {
+    Span<int> index = stackalloc int[10001];
+    var score = 0;
+    var maxScore = 0;
+    var left = 0;
+    var right = 0;
+    while (right < nums.Length)
+    {
+      var num = nums[right];
+      while (left < index[num])
+      {
+        score -= nums[left];
+        index[nums[left]] = 0;
+        left++;
+      }
+      score += num;
+      index[num] = right + 1;
+      maxScore = Math.Max(maxScore, score);
+      right++;
+    }
+    return maxScore;
+  }
+}
+
+[TestFixture]
+public class SolutionTests
+{
+  [TestCase(new[] { 4, 2, 4, 5, 6 }, 17)]
+  [TestCase(new[] { 5, 2, 1, 2, 5, 2, 1, 2, 5 }, 8)]
+  public void Test(int[] nums, int expected)
+  {
+    new Solution().MaximumUniqueSubarray(nums).Should().Be(expected);
+  }
+}
