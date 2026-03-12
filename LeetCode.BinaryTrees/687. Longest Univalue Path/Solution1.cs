6caa0bc0084dcee1eb90e7324f3cb964d6d67a74
Mike Gorodilov <mihailgorodilov@gmail.com>
2026-03-12 10:13:48 +0400

    687. Longest Univalue Path

---
+++ LeetCode.BinaryTrees/687. Longest Univalue Path/Solution1.cs
@@ -0,0 +1,46 @@
+using LeetCode.Common;
+
+namespace LeetCode.BinaryTrees._687._Longest_Univalue_Path;
+
+public class Solution1
+{
+  public int LongestUnivaluePath(TreeNode root)
+  {
+    int longest = 0;
+    F(root);
+    return longest;
+
+    int F(TreeNode node)
+    {
+      if (node == null)
+      {
+        return 0;
+      }
+      int left = F(node.left);
+      int right = F(node.right);
+      int leftPath = 0;
+      int rightPath = 0;
+      if (node.left != null && node.left.val == node.val)
+      {
+        leftPath = left + 1;
+      }
+      if (node.right != null && node.right.val == node.val)
+      {
+        rightPath = right + 1;
+      }
+      longest = Math.Max(longest, leftPath + rightPath);
+      return Math.Max(leftPath, rightPath);
+    }
+  }
+}
+
+[TestFixture]
+public class Solution1Tests
+{
+  [TestCase("[5,4,5,1,1,null,5]", 2)]
+  [TestCase("[1,4,5,4,4,null,5]", 2)]
+  public void Test(string root, int expected)
+  {
+    new Solution1().LongestUnivaluePath(TreeNode.FromString(root)).Should().Be(expected);
+  }
+}
---
+++ LeetCode.BinaryTrees/687. Longest Univalue Path/Solution2.cs
@@ -0,0 +1,47 @@
+using LeetCode.Common;
+
+namespace LeetCode.BinaryTrees._687._Longest_Univalue_Path;
+
+public class Solution2
+{
+  public int LongestUnivaluePath(TreeNode root)
+  {
+    var result = Dfs(root);
+    return result.best;
+
+    (int down, int best) Dfs(TreeNode node)
+    {
+      if (node == null)
+      {
+        return (0, 0);
+      }
+      (int leftDown, int leftBest) = Dfs(node.left);
+      (int rightDown, int rightBest) = Dfs(node.right);
+      int leftPath = 0;
+      int rightPath = 0;
+      if (node.left?.val == node.val)
+      {
+        leftPath = leftDown + 1;
+      }
+      if (node.right?.val == node.val)
+      {
+        rightPath = rightDown + 1;
+      }
+      int down = Math.Max(leftPath, rightPath);
+      int through = leftPath + rightPath;
+      int best = Math.Max(Math.Max(leftBest, rightBest), through);
+      return (down, best);
+    }
+  }
+}
+
+[TestFixture]
+public class Solution2Tests
+{
+  [TestCase("[5,4,5,1,1,null,5]", 2)]
+  [TestCase("[1,4,5,4,4,null,5]", 2)]
+  public void Test(string root, int expected)
+  {
+    new Solution2().LongestUnivaluePath(TreeNode.FromString(root)).Should().Be(expected);
+  }
+}
