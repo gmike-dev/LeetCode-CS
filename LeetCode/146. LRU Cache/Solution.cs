5f48785a0078d5776bfc02330b14dc878f5c20e6
Mike Gorodilov <mihailgorodilov@gmail.com>
2026-04-29 15:25:19 +0400

    146. LRU Cache (refactoring)

--- LeetCode/146. LRU Cache/Solution.cs
+++ LeetCode/146. LRU Cache/Solution.cs
@@ -1,49 +1,58 @@
 namespace LeetCode._146._LRU_Cache;
 
-public class LRUCache
+public class LRUCache(int capacity)
 {
-  private readonly struct CacheItem
+  private class CacheItem(int key, int value)
   {
-    public readonly int Key;
-    public readonly int Value;
-
-    public CacheItem(int key, int value)
-    {
-      Key = key;
-      Value = value;
-    }
+    public readonly int Key = key;
+    public int Value = value;
   }
 
-  private readonly int _capacity;
-  private readonly LinkedList<CacheItem> _list = new();
-  private readonly Dictionary<int, LinkedListNode<CacheItem>> _dict = new ();
-
-  public LRUCache(int capacity)
-  {
-    _capacity = capacity;
-  }
+  private readonly LinkedList<CacheItem> list = [];
+  private readonly Dictionary<int, LinkedListNode<CacheItem>> dict = new(capacity);
 
   public int Get(int key)
   {
-    if (!_dict.TryGetValue(key, out var node))
+    if (!dict.TryGetValue(key, out var node))
       return -1;
 
-    _list.Remove(node);
-    _list.AddFirst(node);
+    list.Remove(node);
+    list.AddFirst(node);
     return node.Value.Value;
   }
 
   public void Put(int key, int value)
   {
-    if (_dict.TryGetValue(key, out var node))
+    if (dict.TryGetValue(key, out var node))
     {
-      _list.Remove(node);
+      node.Value.Value = value;
+      list.Remove(node);
+      list.AddFirst(node);
+      return;
     }
-    else if (_list.Count == _capacity)
+    if (list.Count == capacity)
     {
-      _dict.Remove(_list.Last.Value.Key);
-      _list.RemoveLast();
+      dict.Remove(list.Last.Value.Key);
+      list.RemoveLast();
     }
-    _dict[key] = _list.AddFirst(new CacheItem(key, value));
+    dict[key] = list.AddFirst(new CacheItem(key, value));
+  }
+}
+
+public class LRUCacheTests
+{
+  [Test]
+  public void Test1()
+  {
+    LRUCache lruCache = new(2);
+    lruCache.Put(1, 1); // cache is {1=1}
+    lruCache.Put(2, 2); // cache is {1=1, 2=2}
+    lruCache.Get(1).Should().Be(1);
+    lruCache.Put(3, 3); // LRU key was 2, evicts key 2, cache is {1=1, 3=3}
+    lruCache.Get(2).Should().Be(-1); // returns -1 (not found)
+    lruCache.Put(4, 4); // LRU key was 1, evicts key 1, cache is {4=4, 3=3}
+    lruCache.Get(1).Should().Be(-1); // return -1 (not found)
+    lruCache.Get(3).Should().Be(3); // return 3
+    lruCache.Get(4).Should().Be(4); // return 4
   }
 }
