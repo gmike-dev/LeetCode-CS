6eddf9da88908ad9b56751aed2fc2678612c1ed3
Mike Gorodilov <mihailgorodilov@gmail.com>
2026-04-30 22:56:02 +0400

    432. All One Data Structure (correct solution)

--- LeetCode.Strings/432. All One Data Structure/Solution.cs
+++ LeetCode.Strings/432. All One Data Structure/Solution.cs
@@ -5,60 +5,102 @@ namespace LeetCode.Strings._432._All_One_Data_Structure;
 /// </summary>
 public class AllOne
 {
-  private readonly TrieNode trie = new();
+  private readonly Dictionary<string, int> counter = new();
+  private readonly Dictionary<int, LinkedListNode<LinkedList<string>>> freq = new();
+  private readonly LinkedList<LinkedList<string>> freqList = new();
+  private readonly Dictionary<string, LinkedListNode<string>> dict = new();
 
   public void Inc(string key)
   {
-    var t = trie;
-    foreach (var c in key)
+    if (counter.TryGetValue(key, out int oldFreq))
     {
-      t = t.Next[c - 'a'] ??= new TrieNode();
-      t.Count++;
+      int newFreq = oldFreq + 1;
+      counter[key] = newFreq;
+      var oldFreqBatch = freq[oldFreq];
+      var node = dict[key];
+      oldFreqBatch.Value.Remove(node);
+      if (freq.TryGetValue(newFreq, out var newFreqBatch))
+      {
+        newFreqBatch.Value.AddLast(node);
+      }
+      else
+      {
+        var batch = new LinkedList<string>();
+        batch.AddLast(node);
+        freq[newFreq] = freqList.AddAfter(oldFreqBatch, batch);
+      }
+      if (oldFreqBatch.Value.Count == 0)
+      {
+        freqList.Remove(oldFreqBatch);
+        freq.Remove(oldFreq);
+      }
     }
-    t.Key = key;
+    else
+    {
+      if (freq.TryGetValue(1, out var freqBatch))
+      {
+        dict[key] = freqBatch.Value.AddLast(key);
+      }
+      else
+      {
+        var batch = new LinkedList<string>();
+        dict[key] = batch.AddFirst(key);
+        freq[1] = freqList.AddFirst(batch);
+      }
+      counter[key] = 1;
+    }
   }
 
   public void Dec(string key)
   {
-    var t = trie;
-    foreach (var c in key)
+    int oldFreq = counter[key];
+    if (oldFreq > 1)
     {
-      var i = c - 'a';
-      if (t.Next[i] == null || t.Next[i].Count == 1)
+      int newFreq = oldFreq - 1;
+      counter[key] = newFreq;
+      var oldFreqBatch = freq[oldFreq];
+      var node = dict[key];
+      oldFreqBatch.Value.Remove(node);
+      if (freq.TryGetValue(newFreq, out var newFreqBatch))
       {
-        t.Next[i] = null;
-        break;
+        newFreqBatch.Value.AddLast(node);
       }
-      t = t.Next[i];
-      t.Count--;
+      else
+      {
+        var batch = new LinkedList<string>();
+        batch.AddLast(node);
+        newFreqBatch = freqList.AddBefore(oldFreqBatch, batch);
+        freq[newFreq] = newFreqBatch;
+      }
+      if (oldFreqBatch.Value.Count == 0)
+      {
+        freqList.Remove(oldFreqBatch);
+        freq.Remove(oldFreq);
+      }
+    }
+    else
+    {
+      var node = dict[key];
+      var freqBatch = freq[1];
+      freqBatch.Value.Remove(node);
+      if (freqBatch.Value.Count == 0)
+      {
+        freqList.Remove(freqBatch);
+        freq.Remove(1);
+      }
+      counter.Remove(key);
+      dict.Remove(key);
     }
   }
 
   public string GetMaxKey()
   {
-    for (var t = trie; t != null; t = t.Next.MaxBy(n => n?.Count))
-    {
-      if (t.Key != null)
-        return t.Key;
-    }
-    return "";
+    return freqList.Count == 0 ? "" : freqList.Last.Value.First.Value;
   }
 
   public string GetMinKey()
   {
-    for (var t = trie; t != null; t = t.Next.MinBy(n => n?.Count))
-    {
-      if (t.Key != null)
-        return t.Key;
-    }
-    return "";
-  }
-
-  private class TrieNode
-  {
-    public readonly TrieNode[] Next = new TrieNode[26];
-    public int Count;
-    public string Key;
+    return freqList.Count == 0 ? "" : freqList.First.Value.First.Value;
   }
 }
 
@@ -76,5 +118,54 @@ public class SolutionTests
     allOne.Inc("leet");
     allOne.GetMaxKey().Should().Be("hello"); // return "hello"
     allOne.GetMinKey().Should().Be("leet"); // return "leet"
+  }
+
+  [Test]
+  public void Test2()
+  {
+    var allOne = new AllOne();
+    allOne.Inc("a");
+    allOne.Inc("b");
+    allOne.Inc("b");
+    allOne.Inc("c");
+    allOne.Inc("c");
+    allOne.Inc("c");
+    allOne.Dec("b");
+    allOne.Dec("b");
+    allOne.GetMinKey().Should().Be("a");
+    allOne.GetMaxKey().Should().Be("c");
+    allOne.Dec("a");
+    allOne.GetMaxKey().Should().Be("c");
+    allOne.GetMinKey().Should().Be("c");
+  }
+
+  [Test]
+  public void Test3()
+  {
+    var allOne = new AllOne();
+    allOne.Inc("helloji");
+    allOne.Inc("helloji");
+    allOne.GetMaxKey().Should().Be("helloji");
+    allOne.GetMinKey().Should().Be("helloji");
+    allOne.Inc("hello");
+    allOne.GetMaxKey().Should().Be("helloji");
+    allOne.GetMinKey().Should().Be("hello");
+  }
+
+  [Test]
+  public void Test4()
+  {
+    var allOne = new AllOne();
+    allOne.Inc("a");
+    allOne.Inc("b");
+    allOne.Inc("b");
+    allOne.Inc("c");
+    allOne.Inc("c");
+    allOne.Inc("c");
+    allOne.Dec("b");
+    allOne.Dec("b");
+    allOne.GetMinKey().Should().Be("a");
+    allOne.Dec("a");
+    allOne.GetMinKey().Should().Be("c");
   }
 }
