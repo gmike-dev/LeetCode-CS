fc7a850897d717de83848eb9cb55405f9188385c
Mike Gorodilov <mihailgorodilov@gmail.com>
2026-05-04 10:50:17 +0400

    381. Insert Delete GetRandom O(1) - Duplicates allowed

---
+++ LeetCode/# Design/381. Insert Delete GetRandom O(1) - Duplicates allowed/Solution2.cs
@@ -0,0 +1,123 @@
+namespace LeetCode.__Design._381._Insert_Delete_GetRandom_O_1____Duplicates_allowed.HashSet;
+
+/// <summary>
+/// https://leetcode.com/problems/Insert-delete-GetRandom-o1-duplicates-allowed
+/// </summary>
+public class RandomizedCollection
+{
+  private readonly Dictionary<int, HashSet<int>> indexes = new();
+  private readonly List<int> items = new();
+
+  public bool Insert(int val)
+  {
+    bool exists = indexes.TryGetValue(val, out var indexSet);
+    if (exists)
+    {
+      indexSet.Add(items.Count);
+    }
+    else
+    {
+      indexes[val] = [items.Count];
+    }
+    items.Add(val);
+    return !exists;
+  }
+
+  public bool Remove(int val)
+  {
+    if (!indexes.TryGetValue(val, out var indexSet))
+    {
+      return false;
+    }
+
+    int valIndex = indexSet.First();
+    indexSet.Remove(valIndex);
+
+    int lastIndex = items.Count - 1;
+    int lastItem = items[lastIndex];
+
+    if (valIndex != lastIndex)
+    {
+      items[valIndex] = lastItem;
+      indexes[lastItem].Remove(lastIndex);
+      indexes[lastItem].Add(valIndex);
+    }
+
+    items.RemoveAt(lastIndex);
+
+    if (indexSet.Count == 0)
+    {
+      indexes.Remove(val);
+    }
+
+    return true;
+  }
+
+  public int GetRandom()
+  {
+    return items[Random.Shared.Next(0, items.Count)];
+  }
+}
+
+[TestFixture]
+public class SolutionTests
+{
+  [Test]
+  public void Test1()
+  {
+    RandomizedCollection randomizedCollection = new();
+    randomizedCollection.Insert(1).Should().BeTrue(); // return true since the collection does not contain 1.
+    // Inserts 1 into the collection.
+    randomizedCollection.Insert(1).Should().BeFalse(); // return false since the collection contains 1.
+    // Inserts another 1 into the collection. Collection now contains [1,1].
+    randomizedCollection.Insert(2).Should().BeTrue(); // return true since the collection does not contain 2.
+    // Inserts 2 into the collection. Collection now contains [1,1,2].
+    randomizedCollection.GetRandom().Should().BeInRange(1, 2); // GetRandom should:
+    // - return 1 with probability 2/3, or
+    // - return 2 with probability 1/3.
+    randomizedCollection.Remove(1).Should().BeTrue(); // return true since the collection contains 1.
+    // Removes 1 from the collection. Collection now contains [1,2].
+    randomizedCollection.GetRandom().Should().BeInRange(1, 2); // GetRandom should return 1 or 2, both equally likely.
+  }
+
+  [Test]
+  public void Test2()
+  {
+    RandomizedCollection randomizedCollection = new();
+
+    randomizedCollection.Insert(1); // 1-й insert
+    randomizedCollection.Insert(1); // 2-й insert
+    randomizedCollection.Insert(2); // 3-й insert
+    randomizedCollection.Insert(2); // 4-й insert
+    randomizedCollection.Insert(2); // 5-й insert
+
+    randomizedCollection.Remove(1); // 1-й remove
+    randomizedCollection.Remove(1); // 2-й remove
+    randomizedCollection.Remove(2); // 3-й remove
+
+    randomizedCollection.Insert(1); // 6-й insert
+
+    randomizedCollection.Remove(2); // 4-й remove
+
+    randomizedCollection.GetRandom(); // 1-й getRandom
+    randomizedCollection.GetRandom(); // 2-й getRandom
+    randomizedCollection.GetRandom(); // 3-й getRandom
+    randomizedCollection.GetRandom(); // 4-й getRandom
+    randomizedCollection.GetRandom(); // 5-й getRandom
+    randomizedCollection.GetRandom(); // 6-й getRandom
+    randomizedCollection.GetRandom(); // 7-й getRandom
+    randomizedCollection.GetRandom(); // 8-й getRandom
+    randomizedCollection.GetRandom(); // 9-й getRandom
+    randomizedCollection.GetRandom(); // 10-й getRandom
+  }
+
+  [Test]
+  public void Test3()
+  {
+    RandomizedCollection randomizedCollection = new();
+
+    randomizedCollection.Insert(1); // 1-й insert
+    randomizedCollection.Remove(1); // 1-й remove
+    randomizedCollection.Insert(1); // 2-й insert
+  }
+}
